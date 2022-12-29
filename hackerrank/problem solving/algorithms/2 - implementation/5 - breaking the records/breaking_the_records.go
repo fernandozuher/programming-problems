74c78fd23ab93a331b3ee45862bd2d19c71a0361
fernandozuher <fernando.zuher@gmail.com>
2022-12-29 11:59:55 -0400

    go fmt and new line

--- hackerrank/problem solving/algorithms/2 - implementation/5 - breaking the records/breaking_the_records.go
+++ hackerrank/problem solving/algorithms/2 - implementation/5 - breaking the records/breaking_the_records.go
@@ -3,63 +3,63 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strconv"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-    readLineAsIntArray(scanner)
-    setA := readLineAsIntArray(scanner)
+	readLineAsIntArray(scanner)
+	setA := readLineAsIntArray(scanner)
 
-    result := breakingRecords(setA)
-    fmt.Print(result[0], result[1])
+	result := breakingRecords(setA)
+	fmt.Print(result[0], result[1])
 }
 
     func readLineAsIntArray(scanner *bufio.Scanner) []int {
-        var inputLine string
-        
-        if scanner.Scan() {
-            inputLine = scanner.Text()
-        } else {
-            checkError(scanner.Err())
-        }
+    	var inputLine string
 
-        inputStringArray := strings.Split(inputLine, " ")
-        numbers := make([]int, len(inputStringArray))
+    	if scanner.Scan() {
+    		inputLine = scanner.Text()
+    	} else {
+    		checkError(scanner.Err())
+    	}
 
-        for i, stringNumber := range inputStringArray {
-            number, err := strconv.Atoi(stringNumber)
-            checkError(err)
-            numbers[i] = number
-        }
-        return numbers
+    	inputStringArray := strings.Split(inputLine, " ")
+    	numbers := make([]int, len(inputStringArray))
+
+    	for i, stringNumber := range inputStringArray {
+    		number, err := strconv.Atoi(stringNumber)
+    		checkError(err)
+    		numbers[i] = number
+    	}
+    	return numbers
     }
 
         func checkError(err error) {
-            if err != nil {
-                panic(err)
-            }
+        	if err != nil {
+        		panic(err)
+        	}
         }
 
     func breakingRecords(scores []int) [2]int {
-        least_points, least_points_count := scores[0], 0
-        most_points, most_points_count := scores[0], 0
+    	least_points, least_points_count := scores[0], 0
+    	most_points, most_points_count := scores[0], 0
 
-        for _, score := range scores {
-            if score > most_points {
-                most_points = score
-                most_points_count++
-            } else if score < least_points {
-                least_points = score
-                least_points_count++
-            }
-        }
+    	for _, score := range scores {
+    		if score > most_points {
+    			most_points = score
+    			most_points_count++
+    		} else if score < least_points {
+    			least_points = score
+    			least_points_count++
+    		}
+    	}
 
-        records := [2]int{most_points_count, least_points_count}
-        return records;
+    	records := [2]int{most_points_count, least_points_count}
+    	return records
     }
