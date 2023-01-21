92c107e1793dc44498fb17bae3c9ac37066f1dc6
fernandozuher <fernando.zuher@gmail.com>
2023-01-21 18:06:59 -0300

    improved c implementation

--- hackerrank/problem solving/algorithms/2 - implementation/22 - angry professor/angry_professor.c
+++ hackerrank/problem solving/algorithms/2 - implementation/22 - angry professor/angry_professor.c
@@ -2,52 +2,46 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int read_one_int();
-char* angry_professor();
-int count_late_arrival_time(const int n_students, const int cancellation_threshold);
+bool angry_professor();
+    int count_early_arrival_time(const int n_students);
 
 
 int main() {
-    int nTestCases = read_one_int();
+    const int nTestCases = read_one_int();
+    bool nResults[nTestCases];
 
-    char *nResults[nTestCases];
-
-    for (int i = 0; i < nTestCases; ++i)
-        nResults[i] = angry_professor();
-
-    for (int i = 0; i < nTestCases; ++i)
-        puts(nResults[i]);
+    for (int i = 0; i < nTestCases; nResults[i++] = angry_professor());
+    for (int i = 0; i < nTestCases; puts(nResults[i++] ? "YES" : "NO"));
 
     return 0;
 }
 
-int read_one_int() {
-    int n;
-    char white_space_or_eof;
-    scanf("%d%c", &n, &white_space_or_eof);
-    return n;
-}
-
-char* angry_professor() {
-    const int n_students = read_one_int();
-    const int cancellation_threshold = read_one_int();
-
-    const int late_arrival_time_count = count_late_arrival_time(n_students, cancellation_threshold);
-
-    char *is_class_happening = late_arrival_time_count < cancellation_threshold ? "YES" : "NO";
-    return is_class_happening;
-}
-
-int count_late_arrival_time(const int n_students, const int cancellation_threshold) {
-    int late_arrival_time_count = 0;
-
-    for (int i = 0; i < n_students; i++) {
-        const int student_arrival_time = read_one_int();
-
-        if (student_arrival_time > 0)
-            late_arrival_time_count++;
+    int read_one_int() {
+        int number;
+        char white_space_or_eof;
+        scanf("%d%c", &number, &white_space_or_eof);
+        return number;
     }
 
-    return late_arrival_time_count;
-}
+    bool angry_professor() {
+        const int _n_students_arrival_time = read_one_int();
+        const int cancellation_threshold = read_one_int();
+        const bool is_class_cancelled = count_early_arrival_time(_n_students_arrival_time) < cancellation_threshold;
+        return is_class_cancelled;
+    }
+
+        int count_early_arrival_time(const int n_students) {
+            int early_arrival_time_count = 0;
+
+            for (int i = 0, student_arrival_time; i < n_students; i++) {
+                student_arrival_time = read_one_int();
+
+                if (student_arrival_time <= 0)
+                    early_arrival_time_count++;
+            }
+
+            return early_arrival_time_count;
+        }
