6476d4292294ad2c39b61cb18532b99f7687330b
fernandozuher <fernando.zuher@gmail.com>
2023-02-02 18:02:14 -0300

    java and c# implementations

---
+++ codility/lessons/1 - iterations/binary gap/binary_gap.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    private string _binary;
+    private int _longestBinaryGap;
+
+    public int solution(int N)
+    {
+        _binary = _ConvertIntToBinaryString(N);
+        _longestBinaryGap = _FindLongestBinaryGap();
+        return _longestBinaryGap;
+    }
+
+        private string _ConvertIntToBinaryString(int N)
+        {
+            string binary = Convert.ToString(N, 2);
+            return binary;
+        }
+
+        private int _FindLongestBinaryGap()
+        {
+            int longestBinaryGap = 0;
+
+            for (List<int> index = new List<int>(new int[3]); !index.Contains(-1);)
+            {
+                int gapLength = _FindGapLength(index);
+                longestBinaryGap = Math.Max(gapLength, longestBinaryGap);
+            }
+
+            return longestBinaryGap;
+        }
+
+            private int _FindGapLength(List<int> index)
+            {
+                index[0] = _binary.IndexOf('1', index[1]);
+                if (index[0] != -1)
+                {
+                    index[1] = _binary.IndexOf('0', index[0] + 1);
+                    if (index[1] != -1)
+                    {
+                        index[2] = _binary.IndexOf('1', index[1] + 1);
+                        if (index[2] != -1)
+                        {
+                            int gapLength = index[2] - index[1];
+                            return gapLength;
+                        }
+                        else
+                            return 0;
+                    }
+                    else
+                        return 0;
+                }
+                else
+                    return 0;
+            }
+}
--- codility/lessons/1 - iterations/binary gap/binary_gap.java
+++ codility/lessons/1 - iterations/binary gap/binary_gap.java
@@ -1,11 +1,47 @@
+import java.util.*;
 
+public class Solution {
+    private String _binary;
+    private int _longestBinaryGap;
 
-class Solution {
     public int solution(int N) {
-        final String BINARY = Integer.toBinaryString(N);
-        int i = 0, j = 0, max_gap = 0;
-        while ((i = BINARY.indexOf('1', j)) >= 0 && (j = BINARY.indexOf('1', i+1)) > 0)
-            max_gap = Math.max(j - i - 1, max_gap);
-        return max_gap;
+        _binary = _convertIntToBinaryString(N);
+        _longestBinaryGap = _findLongestBinaryGap();
+        return _longestBinaryGap;
     }
+
+        private String _convertIntToBinaryString(int N) {
+            return Integer.toBinaryString(N);
+        }
+
+        private int _findLongestBinaryGap() {
+            int longestBinaryGap = 0;
+
+            for (List<Integer> index = new ArrayList<Integer>(Arrays.asList(0, 0, 0)); !index.contains(-1);) {
+                final int GAP_LENGTH = _findGapLength(index);
+                longestBinaryGap = Math.max(GAP_LENGTH, longestBinaryGap);
+            }
+
+            return longestBinaryGap;
+        }
+
+            private int _findGapLength(List<Integer> index) {
+                index.set(0, _binary.indexOf("1", index.get(1)));
+
+                if (index.get(0) != -1) {
+                    index.set(1, _binary.indexOf("0", index.get(0) + 1));
+
+                    if (index.get(1) != -1) {
+                        index.set(2, _binary.indexOf("1", index.get(1) + 1));
+
+                        if (index.get(2) != -1) {
+                            final int GAP_LENGTH = index.get(2) - index.get(1);
+                            return GAP_LENGTH;
+                        } else
+                            return 0;
+                    } else
+                        return 0;
+                } else
+                    return 0;
+            }
 }
