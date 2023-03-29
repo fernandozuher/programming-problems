f2bb81caa82b9775062e6445454683a440a256f2
fernandozuher <fernando.zuher@gmail.com>
2023-03-29 18:49:58 -0300

    Jumping on the Clouds: Revisited

---
+++ hackerrank/problem solving/algorithms/2 - implementation/28 - jumping on the clouds - revisited/jumping_on_the_clouds_revisited.c
@@ -0,0 +1,65 @@
+// Source: https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int read_a_number();
+int* read_an_array(const int SIZE);
+int jumping_on_clouds(const int *ARRAY, const int SIZE_ARRAY, const int JUMP_LENGTH);
+    bool is_cloud_cumulus(const int CLOUD);
+    int generate_new_cloud_index(const int CLOUD_INDEX, const int SIZE_ARRAY, const int JUMP_LENGTH);
+    bool is_cloud_index_back_to_first_cloud(const int CLOUD_INDEX);
+
+
+int main() {
+    const int SIZE_ARRAY = read_a_number();
+    const int JUMP_LENGTH = read_a_number();
+    int *array = read_an_array(SIZE_ARRAY);
+
+    const int ENERGY = jumping_on_clouds(array, SIZE_ARRAY, JUMP_LENGTH);
+    printf("%d\n", ENERGY);
+
+    free(array);
+    array = NULL;
+
+    return 0;
+}
+
+    int read_a_number() {
+        int number;
+        scanf("%d", &number);
+        return number;
+    }
+
+    int* read_an_array(const int SIZE_ARRAY) {
+        int *array = (int*) calloc(SIZE_ARRAY, sizeof(int));
+        for (int i = 0; i < SIZE_ARRAY; i++)
+            scanf("%d", &array[i]);
+        return array;
+    }
+
+    int jumping_on_clouds(const int *ARRAY, const int SIZE_ARRAY, const int JUMP_LENGTH) {
+        int energy = 100;
+
+        for (int cloud_index = 0; true; ) {
+            energy -= is_cloud_cumulus(ARRAY[cloud_index]) ? 1 : 3;
+            cloud_index = generate_new_cloud_index(cloud_index, SIZE_ARRAY, JUMP_LENGTH);
+            if (is_cloud_index_back_to_first_cloud(cloud_index))
+                break;
+        }
+
+        return energy;
+    }
+
+        bool is_cloud_cumulus(const int CLOUD) {
+            return CLOUD == 0;
+        }
+
+        int generate_new_cloud_index(const int CLOUD_INDEX, const int SIZE_ARRAY, const int JUMP_LENGTH) {
+            return (CLOUD_INDEX + JUMP_LENGTH) % SIZE_ARRAY;
+        }
+
+        bool is_cloud_index_back_to_first_cloud(const int CLOUD_INDEX) {
+            return CLOUD_INDEX == 0;
+        }
---
+++ hackerrank/problem solving/algorithms/2 - implementation/28 - jumping on the clouds - revisited/jumping_on_the_clouds_revisited.go
@@ -0,0 +1,60 @@
+// Source: https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem?isFullScreen=true
+
+package main
+
+import "fmt"
+
+func main() {
+    var sizeArray int = ReadANumber()
+    var jumpLength int = ReadANumber()
+    var array []int = ReadAnArray(sizeArray)
+
+    var energy int = JumpingOnClouds(array, jumpLength)
+    fmt.Println(energy)
+}
+
+func ReadANumber() int {
+    var number int
+    fmt.Scan(&number)
+    return number
+}
+
+func ReadAnArray(sizeArray int) []int {
+    var array []int = make([]int, sizeArray)
+    for i := 0; i < sizeArray; i++ {
+        fmt.Scan(&array[i])
+    }
+    return array
+}
+
+func JumpingOnClouds(array []int, jumpLength int) int {
+    var energy int = 100
+
+    for cloudIndex := 0; true; {
+        var isCloudCumulus bool = array[cloudIndex] == 0
+        energy -= SpentEnergyAccordingToTypeOfCloud(isCloudCumulus)
+
+        cloudIndex = GenerateNewCloudIndex(cloudIndex, len(array), jumpLength)
+        if IsCloudIndexBackToFirstCloud(cloudIndex) {
+            break
+        }
+    }
+
+    return energy
+}
+
+func SpentEnergyAccordingToTypeOfCloud(isCloudCumulus bool) int {
+    if isCloudCumulus {
+        return 1
+    } else {
+        return 3
+    }
+}
+
+func GenerateNewCloudIndex(cloudIndex int, sizeArray int, jumpLength int) int {
+    return (cloudIndex + jumpLength) % sizeArray
+}
+
+func IsCloudIndexBackToFirstCloud(cloudIndex int) bool {
+    return cloudIndex == 0
+}
