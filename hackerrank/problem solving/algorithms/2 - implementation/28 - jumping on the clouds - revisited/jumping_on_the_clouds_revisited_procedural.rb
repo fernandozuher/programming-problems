41cc5cd05f9a507f22bffad064acba4415c2dd28
fernandozuher <fernando.zuher@gmail.com>
2023-03-30 18:04:55 -0300

    fixed indentation

--- hackerrank/problem solving/algorithms/2 - implementation/28 - jumping on the clouds - revisited/jumping_on_the_clouds_revisited_procedural.rb
+++ hackerrank/problem solving/algorithms/2 - implementation/28 - jumping on the clouds - revisited/jumping_on_the_clouds_revisited_procedural.rb
@@ -10,36 +10,35 @@ def main
     puts energy
 end
 
-def readAnArray
-    array = gets.split.map(&:to_i)
-end
-
-def jumpingOnClouds array, jumpLength
-    energy = 100
-
-    cloudIndex = 0
-    while true
-        energy -= spentEnergyAccordingToTypeOfCloud array[cloudIndex]
-        cloudIndex = generateNewCloudIndex cloudIndex, array.size, jumpLength
-        if isCloudIndexBackToFirstCloud cloudIndex
-            break
-        end
+    def readAnArray
+        array = gets.split.map(&:to_i)
     end
 
-    energy
-end
+    def jumpingOnClouds array, jumpLength
+        energy = 100
 
+        cloudIndex = 0
+        while true
+            energy -= spentEnergyAccordingToTypeOfCloud array[cloudIndex]
+            cloudIndex = generateNewCloudIndex cloudIndex, array.size, jumpLength
+            if isCloudIndexBackToFirstCloud cloudIndex
+                break
+            end
+        end
 
-def spentEnergyAccordingToTypeOfCloud cloudType
-    return cloudType == 0 ? 1 : 3
-end
+        energy
+    end
 
-def generateNewCloudIndex cloudIndex, sizeArray, jumpLength
-    return (cloudIndex + jumpLength) % sizeArray
-end
+        def spentEnergyAccordingToTypeOfCloud cloudType
+            return cloudType == 0 ? 1 : 3
+        end
 
-def isCloudIndexBackToFirstCloud cloudIndex
-    return cloudIndex == 0
-end
+        def generateNewCloudIndex cloudIndex, sizeArray, jumpLength
+            return (cloudIndex + jumpLength) % sizeArray
+        end
+
+        def isCloudIndexBackToFirstCloud cloudIndex
+            return cloudIndex == 0
+        end
 
 main
