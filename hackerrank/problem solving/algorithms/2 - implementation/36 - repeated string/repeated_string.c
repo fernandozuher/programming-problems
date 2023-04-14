0980a2439f256ea773f854ed47fa345c69bdc657
fernandozuher <fernando.zuher@gmail.com>
2023-04-14 17:58:02 -0300

    repeated string

---
+++ hackerrank/problem solving/algorithms/2 - implementation/36 - repeated string/repeated_string.c
@@ -0,0 +1,80 @@
+// Source: https://www.hackerrank.com/challenges/repeated-string/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char* read_a_string();
+unsigned long read_a_number();
+unsigned long find_quantity_of_a_in_repeated_string(char* string, const unsigned long N_CHARACTERS);
+unsigned long get_size_string(char* string);
+unsigned long find_quantity_of_a_in_original_string(char* string);
+unsigned long find_quantity_of_remaining_a(char* string, const unsigned long N_REMAINING_CHARACTERS);
+char* free_string(char* string);
+
+int main() {
+    char* input_string = read_a_string();
+    const unsigned long N_CHARACTERS = read_a_number();
+
+    const unsigned long QUANTITY_OF_A = find_quantity_of_a_in_repeated_string(input_string, N_CHARACTERS);
+    printf("%llu\n", QUANTITY_OF_A);
+
+    input_string = free_string(input_string);
+
+    return 0;
+}
+
+char* read_a_string() {
+    const unsigned long INPUT_STRING_MAX_SIZE = 103;
+    char* input_string = (char*) calloc(INPUT_STRING_MAX_SIZE, sizeof(char));
+    scanf("%s", input_string);
+    return input_string;
+}
+
+unsigned long read_a_number() {
+    unsigned long number;
+    scanf("%llu", &number);
+    return number;
+}
+
+unsigned long find_quantity_of_a_in_repeated_string(char* string, const unsigned long N_CHARACTERS) {
+    const unsigned long SIZE_STRING = get_size_string(string);
+    unsigned long quantity_of_a = find_quantity_of_a_in_original_string(string);
+
+    const unsigned long N_STRINGS = N_CHARACTERS / SIZE_STRING;
+    quantity_of_a *= N_STRINGS;
+    quantity_of_a += find_quantity_of_remaining_a(string, N_CHARACTERS % SIZE_STRING);
+
+    return quantity_of_a;
+}
+
+unsigned long get_size_string(char* string) {
+    unsigned long size = 0;
+    while (*(string++) != '\0')
+        size++;
+    return size;
+}
+
+unsigned long find_quantity_of_a_in_original_string(char* string) {
+    unsigned long quantity_of_a = 0;
+
+    for (; *string != '\0'; string++)
+        if (*string == 'a')
+            quantity_of_a++;
+
+    return quantity_of_a;
+}
+
+unsigned long find_quantity_of_remaining_a(char* string, const unsigned long N_REMAINING_CHARACTERS) {
+    unsigned long quantity_of_a = 0;
+
+    for (unsigned long i = 0; i < N_REMAINING_CHARACTERS; i++)
+        if (*(string++) == 'a')
+            quantity_of_a++;
+
+    return quantity_of_a;
+}
+
+char* free_string(char* string) {
+    free(string);
+    return NULL;
+}
