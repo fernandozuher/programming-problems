78ddcea6dc7481a37156cee72ec5faa361d091e2
fernandozuher <fernando.zuher@gmail.com>
2023-07-14 23:10:36 -0300

    refactored

--- codility/lessons/4 - counting elements/max counters/max_counters.cpp
+++ codility/lessons/4 - counting elements/max counters/max_counters.cpp
@@ -4,14 +4,13 @@
 
 using namespace std;
 
-vector<int> solution(int n_counters, vector<int>& array)
+vector<int> solution(const int n_counters, vector<int>& array)
 {
     bool found_max_counter {false};
     int max {0};
     valarray<int> counters(n_counters + 1);
 
     for (int current_max {0}; const auto value : array)
-
         if (value <= n_counters) {
             if (found_max_counter) {
                 counters = 0;
