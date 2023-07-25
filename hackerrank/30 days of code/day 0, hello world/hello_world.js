1e32160601361529cee9491b1808115780a05836
fernandozuher <fernando.zuher@gmail.com>
2023-07-25 14:00:57 -0300

    changed code order

--- hackerrank/30 days of code/day 0, hello world/hello_world.js
+++ hackerrank/30 days of code/day 0, hello world/hello_world.js
@@ -1,10 +1,5 @@
 // https://www.hackerrank.com/challenges/30-hello-world/problem?isFullScreen=true
 
-function processData(inputString) {
-    console.log("Hello, World.");
-    console.log(inputString);
-}
-
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 
@@ -17,3 +12,8 @@ process.stdin.on("data", function(input) {
 process.stdin.on("end", function() {
     processData(_input);
 });
+
+function processData(inputString) {
+    console.log("Hello, World.");
+    console.log(inputString);
+}
