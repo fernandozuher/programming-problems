d6edeb04e1f35d9f8f6685c948e9783962df3988
fernandozuher <fernando.zuher@gmail.com>
2023-07-26 20:24:55 -0300

    refactored

---
+++ hackerrank/30 days of code/day 1, data types/data_types.c
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/30-data-types/problem?isFullScreen=true
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+
+    int i2;
+    double d2;
+    char s2[50];
+    scanf("%d %lf %49[^\n]", &i2, &d2, s2);
+
+    printf("%d\n", i + i2);
+    printf("%.1lf\n", d + d2);
+    printf("%s%s\n", s, s2);
+
+    return 0;
+}
