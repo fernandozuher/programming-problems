c938b4b8d0c09e8da484627ac07c52f7491cdd3d
fernandozuher <fernando.zuher@gmail.com>
2023-08-01 22:19:02 -0300

    refactored

--- hackerrank/30 days of code/day 6, lets review/lets_review.c
+++ hackerrank/30 days of code/day 6, lets review/lets_review.c
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/challenges/30-review-loop/problem?isFullScreen=true
+
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -7,14 +9,14 @@ int main() {
     scanf("%d", &n);
 
     for (char word[10000]; scanf("%s", word) != EOF; ) {
-        
+
         char word1[5001], word2[5001];
         int size_word1 = 0, size_word2 = 0;
         bool flag = true;
 
         for (int i = 0; word[i] != '\0'; ++i, flag = !flag)
             flag ? (word1[size_word1++] = word[i]) : (word2[size_word2++] = word[i]);
-        
+
         word1[size_word1] = word2[size_word2] = '\0';
         printf("%s %s\n", word1, word2);
     }
