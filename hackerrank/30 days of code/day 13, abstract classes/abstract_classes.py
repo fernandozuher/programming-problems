d920b197cd547650c0f620c5c3456fe0b4d53716
fernandozuher <fernando.zuher@gmail.com>
2023-08-16 09:12:17 -0300

    refactored

--- hackerrank/30 days of code/day 13, abstract classes/abstract_classes.py
+++ hackerrank/30 days of code/day 13, abstract classes/abstract_classes.py
@@ -1,27 +1,43 @@
+# https://www.hackerrank.com/challenges/30-abstract-classes/problem?isFullScreen=true
+
 from abc import ABCMeta, abstractmethod
+
+def main():
+
+    TITLE = input()
+    AUTHOR = input()
+    PRICE = int(input())
+    
+    NOVEL = MyBook(title, author, price)
+    NOVEL.display()
 
 
 class Book(object, metaclass=ABCMeta):
+
     def __init__(self, title, author):
+
         self.title = title
         self.author = author
 
+
     @abstractmethod
     def display():
+
         pass
 
 
 class MyBook(Book):
+
     def __init__(self, title, author, price):
+
         super().__init__(title, author)
         self.price = price
 
+
     def display(self):
+
         print(f"Title: {self.title} \nAuthor: {self.author} \nPrice: {self.price}")
 
 
-title = input()
-author = input()
-price = int(input())
-new_novel = MyBook(title, author, price)
-new_novel.display()
+if __name__ == '__main__':
+    main()
--- hackerrank/30 days of code/day 13, abstract classes/abstract_classes.rb
+++ hackerrank/30 days of code/day 13, abstract classes/abstract_classes.rb
@@ -1,34 +1,40 @@
-## This is a **simulation** of an abstract class provided at user request. ##
-class Book
-    attr_accessor :title
-    attr_accessor :author
-    
-    def initialize(title, author)
-        raise 'You cannot instantiate an abstract class.'
-    end
-    
-    def display
-        raise 'You must override this method in your implementing class.'
-    end
+# https://www.hackerrank.com/challenges/30-abstract-classes/problem?isFullScreen=true
+
+def main:
+    title = gets.rstrip
+    author = gets.rstrip
+    price = gets.rstrip
+
+    novel = MyBook.new(title, author, price)
+    novel.display
 end
 
-class MyBook < Book
-    attr_accessor :price
-
-    def initialize(title, author, price)
-        @title = title
-        @author = author
-        @price = price
+    # This is a **simulation** of an abstract class provided at user request. #
+    class Book
+        attr_accessor :title
+        attr_accessor :author
+        
+        def initialize(title, author)
+            raise 'You cannot instantiate an abstract class.'
+        end
+        
+        def display
+            raise 'You must override this method in your implementing class.'
+        end
     end
 
-    def display
-        puts "Title: #{title} \nAuthor: #{author} \nPrice: #{price}"
-    end
-end
+        class MyBook < Book
+            attr_accessor :price
 
-title = gets
-author = gets
-price = gets
+            def initialize(title, author, price)
+                @title = title
+                @author = author
+                @price = price
+            end
 
-new_novel = MyBook.new(title, author, price)
-new_novel.display
+            def display
+                puts "Title: #{title} \nAuthor: #{author} \nPrice: #{price}"
+            end
+        end
+
+main
