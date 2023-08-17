80c40e94e8203aab2ce473af7f878d1592070e58
fernandozuher <fernando.zuher@gmail.com>
2023-08-17 11:26:25 -0300

    refactored

--- hackerrank/30 days of code/day 0, hello world/HelloWorld.java
+++ hackerrank/30 days of code/day 0, hello world/HelloWorld.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Solution {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         Scanner scan = new Scanner(System.in);
