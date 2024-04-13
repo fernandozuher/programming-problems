fc4461aba228b56cc1f53c44a3eacc5810174539
fernandozuher <fernando.zuher@gmail.com>
2024-04-13 11:47:02 -0300

    C++20 views::values replaced lambda

--- hackerrank/problem solving/algorithms/2 - implementation/38 - equalize the array/equalize_the_array.cpp
+++ hackerrank/problem solving/algorithms/2 - implementation/38 - equalize the array/equalize_the_array.cpp
@@ -1,8 +1,9 @@
 // https://www.hackerrank.com/challenges/equality-in-a-array/problem?isFullScreen=true
 
-import <algorithm>;
-import <iostream>;
-import <map>;
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <ranges>
 
 using namespace std;
 
@@ -21,8 +22,7 @@ int main()
 
     int equalize_array(const map<int, int>& array, const int n)
     {
-        auto cmp {[](const auto& p1, const auto& p2) {return p1.second < p2.second;}};
-        int maximum_quantity_of_equal_element {ranges::max_element(array, cmp)->second};
+        int maximum_quantity_of_equal_element = *ranges::max_element(views::values(array));
         int minimum_number_required_deletions {n - maximum_quantity_of_equal_element};
         return minimum_number_required_deletions;
     }
