ab9ea5687ee4317c2c2ff9ae51ba15fd8f2c1fb2
fernandozuher <fernando.zuher@gmail.com>
2024-04-15 15:22:43 -0300

    refactored

--- hackerrank/problem solving/algorithms/2 - implementation/40 - acm icpc team/acm_icpc_team.c
+++ hackerrank/problem solving/algorithms/2 - implementation/40 - acm icpc team/acm_icpc_team.c
@@ -1,106 +1,93 @@
-// Source: https://www.hackerrank.com/challenges/acm-icpc-team/problem?isFullScreen=true
+// https://www.hackerrank.com/challenges/acm-icpc-team/problem?isFullScreen=true
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int read_a_number();
-char** read_binary_strings(const int N_STRINGS, const int SIZE_STRING);
-    char* read_a_string(const int SIZE);
+char **read_strings(const int n, const int size_string);
+int *find_maximum_subjects_and_teams_that_know_them(char **binary_strings, const int n);
+    int count_subjects_known_by_2_teams(char *binary_string_1, char *binary_string_2);
+    void update_maximum_subjects_and_teams_that_know_them(
+        const int subjects_known_by_2_teams,
+        int *maximum_subjects_known_by_teams,
+        int *teams_that_know_maximum_subjects);
+void print_array(const int *array, const int size);
+char **free_string_array(char **array, int n);
 
-int* find_maximum_subjects_and_teams_that_know_them(char** binary_strings, const int SIZE_ARRAY);
-    int count_subjects_known_by_2_teams(char* binary_string_1, char* binary_string_2);
-    void update_maximum_subjects_and_teams_that_know_them(const int SUBJECTS_KNOWN_BY_2_TEAMS, int* maximum_subjects_known_by_teams, int* teams_that_know_maximum_subjects);
+int main()
+{
+    int attendees, topics;
+    scanf("%d %d", &attendees, &topics);
+    char **binary_strings = read_strings(attendees, topics);
 
-void print_array(const int* array, const int SIZE);
-char** free_strings_array(char** array, const int SIZE);
-int* free_int_array(int* array);
-
-
-int main() {
-    const int ATTENDEES = read_a_number();
-    const int TOPICS = read_a_number();
-    char** binary_strings = read_binary_strings(ATTENDEES, TOPICS);
-
-    int* output = find_maximum_subjects_and_teams_that_know_them(binary_strings, ATTENDEES);
+    int *output = find_maximum_subjects_and_teams_that_know_them(binary_strings, attendees);
     print_array(output, 2);
 
-    binary_strings = free_strings_array(binary_strings, ATTENDEES);
-    output = free_int_array(output);
+    binary_strings = free_string_array(binary_strings, attendees);
+    free(output);
+    output = NULL;
 
     return 0;
 }
 
-    int read_a_number() {
-        int number;
-        scanf("%d", &number);
-        return number;
-    }
-
-    char** read_binary_strings(const int N_STRINGS, const int SIZE_STRING) {
-        char** binary_strings = (char**) calloc(N_STRINGS, sizeof(char*));
-
-        for (int i = 0; i < N_STRINGS; i++)
-            binary_strings[i] = read_a_string(SIZE_STRING);
-
-        return binary_strings;
-    }
-
-        char* read_a_string(const int SIZE) {
-            char* string = (char*) calloc(SIZE + 1, sizeof(char));
-            scanf("%s", string);
-            return string;
+    char **read_strings(const int n, const int size_string)
+    {
+        char **array = (char**) calloc(n, sizeof(char*));
+        for (int i = 0; i < n; ++i) {
+            array[i] = (char*) calloc(size_string + 1, sizeof(char)); // + \0
+            scanf("%s", array[i]);
         }
+        return array;
+    }
 
-    int* find_maximum_subjects_and_teams_that_know_them(char** binary_strings, const int SIZE_ARRAY) {
+    int *find_maximum_subjects_and_teams_that_know_them(char **binary_strings, const int n)
+    {
         int maximum_subjects_known_by_teams = 0;
         int teams_that_know_maximum_subjects = 0;
 
-        for (int i = 0; i < SIZE_ARRAY - 1; i++) {
-            for (int j = i + 1; j < SIZE_ARRAY; j++) {
-                const int SUBJECTS_KNOWN_BY_2_TEAMS = count_subjects_known_by_2_teams(binary_strings[i], binary_strings[j]);
-                update_maximum_subjects_and_teams_that_know_them(SUBJECTS_KNOWN_BY_2_TEAMS, &maximum_subjects_known_by_teams, &teams_that_know_maximum_subjects);
+        for (int i = 0, n1 = n - 1; i < n1; ++i)
+            for (int j = i + 1; j < n; ++j) {
+                int subjects_known_by_2_teams = count_subjects_known_by_2_teams(binary_strings[i], binary_strings[j]);
+                update_maximum_subjects_and_teams_that_know_them(subjects_known_by_2_teams, &maximum_subjects_known_by_teams, &teams_that_know_maximum_subjects);
             }
-        }
 
-        int* output = (int*) calloc(2, sizeof(int));
+        int *output = (int*) calloc(2, sizeof(int));
         output[0] = maximum_subjects_known_by_teams;
         output[1] = teams_that_know_maximum_subjects;
 
         return output;
     }
 
-        int count_subjects_known_by_2_teams(char* binary_string_1, char* binary_string_2) {
+        int count_subjects_known_by_2_teams(char *binary_string_1, char *binary_string_2)
+        {
             int subjects_known_by_2_teams = 0;
-
-            for (int i = 0; binary_string_1[i] != '\0'; i++)
+            for (int i = 0, n = strlen(binary_string_1); i < n; ++i)
                 if (binary_string_1[i] == '1' || binary_string_2[i] == '1')
-                    subjects_known_by_2_teams++;
-
+                    ++subjects_known_by_2_teams;
             return subjects_known_by_2_teams;
         }
 
-        void update_maximum_subjects_and_teams_that_know_them(const int SUBJECTS_KNOWN_BY_2_TEAMS, int* maximum_subjects_known_by_teams, int* teams_that_know_maximum_subjects) {
-            if (SUBJECTS_KNOWN_BY_2_TEAMS > *maximum_subjects_known_by_teams) {
-                *maximum_subjects_known_by_teams = SUBJECTS_KNOWN_BY_2_TEAMS;
+        void update_maximum_subjects_and_teams_that_know_them(
+            const int subjects_known_by_2_teams,
+            int *maximum_subjects_known_by_teams,
+            int *teams_that_know_maximum_subjects)
+        {
+            if (subjects_known_by_2_teams > *maximum_subjects_known_by_teams) {
+                *maximum_subjects_known_by_teams = subjects_known_by_2_teams;
                 *teams_that_know_maximum_subjects = 1;
             }
-            else if (SUBJECTS_KNOWN_BY_2_TEAMS == *maximum_subjects_known_by_teams)
-                (*teams_that_know_maximum_subjects)++;
+            else if (subjects_known_by_2_teams == *maximum_subjects_known_by_teams)
+                ++(*teams_that_know_maximum_subjects);
         }
 
-    void print_array(const int* array, const int SIZE) {
-        for (int i = 0; i < SIZE; printf("%d\n", array[i++]));
+    void print_array(const int *array, const int n)
+    {
+        for (int i = 0; i < n; printf("%d\n", array[i++]));
     }
 
-    char** free_strings_array(char** array, const int SIZE) {
-        for (int i = 0; i < SIZE; i++)
-            free(array[i]);
-
-        free(array);
-        return NULL;
-    }
-
-    int* free_int_array(int* array) {
+    char **free_string_array(char **array, int n)
+    {
+        for (; n--; free(array[n]));
         free(array);
         return NULL;
     }
