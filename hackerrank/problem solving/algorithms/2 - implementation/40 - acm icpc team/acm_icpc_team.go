2e64e2dc41a6153c09ff60d0ad7b27dd1935fb1e
fernandozuher <fernando.zuher@gmail.com>
2024-04-16 13:33:17 -0300

    refactored

--- hackerrank/problem solving/algorithms/2 - implementation/40 - acm icpc team/acm_icpc_team.go
+++ hackerrank/problem solving/algorithms/2 - implementation/40 - acm icpc team/acm_icpc_team.go
@@ -1,62 +1,50 @@
-// Source: https://www.hackerrank.com/challenges/acm-icpc-team/problem?isFullScreen=true
+// https://www.hackerrank.com/challenges/acm-icpc-team/problem?isFullScreen=true
 
 package main
 
 import "fmt"
 
 func main() {
-    var attendees uint32 = ReadANumber()
-    var _ uint32 = ReadANumber()
-    var binaryStrings []string = ReadBinaryStrings(attendees)
-
-    var output [2]uint32 = FindMaximumSubjectsAndTeamsThatKnowThem(binaryStrings)
-    PrintArray(output)
+    var attendees, topics int
+    fmt.Scan(&attendees, &topics)
+    var binaryStrings []string = readBinaryStrings(attendees)
+    var maximumSubjectsKnownByTeams, teamsThatKnowMaximumSubjects int = findMaximumSubjectsAndTeamsThatKnowThem(binaryStrings)
+    fmt.Printf("%d\n%d", maximumSubjectsKnownByTeams, teamsThatKnowMaximumSubjects)
 }
 
-    func ReadANumber() uint32 {
-        var number uint32
-        fmt.Scan(&number)
-        return number
-    }
-
-    func ReadBinaryStrings(nStrings uint32) []string {
-        var binaryStrings []string = make([]string, nStrings)
-
-        for i, _ := range binaryStrings {
+    func readBinaryStrings(n int) []string {
+        binaryStrings := make([]string, n)
+        for i := range binaryStrings {
             fmt.Scan(&binaryStrings[i])
         }
-
         return binaryStrings
     }
 
-    func FindMaximumSubjectsAndTeamsThatKnowThem(binaryStrings []string) [2]uint32 {
-        var maximumSubjectsKnownByTeams uint32 = 0
-        var teamsThatKnowMaximumSubjects uint32 = 0
+    func findMaximumSubjectsAndTeamsThatKnowThem(binaryStrings []string) (int, int) {
+        maximumSubjectsKnownByTeams := 0
+        teamsThatKnowMaximumSubjects := 0
 
-        for i := 0; i < len(binaryStrings)-1; i++ {
-            for j := (i + 1); j < len(binaryStrings); j++ {
-                var subjectsKnownBy2Teams uint32 = CountSubjectsKnownBy2Teams(binaryStrings[i], binaryStrings[j])
-                UpdateMaximumSubjectsAndTeamsThatKnowThem(subjectsKnownBy2Teams, &maximumSubjectsKnownByTeams, &teamsThatKnowMaximumSubjects)
+        for i, previous := range binaryStrings[:len(binaryStrings)-1] {
+            for _, current := range binaryStrings[i+1:] {
+                var subjectsKnownBy2Teams int = countSubjectsKnownBy2Teams(previous, current)
+                updateMaximumSubjectsAndTeamsThatKnowThem(subjectsKnownBy2Teams, &maximumSubjectsKnownByTeams, &teamsThatKnowMaximumSubjects)
             }
         }
 
-        var output [2]uint32 = [2]uint32{maximumSubjectsKnownByTeams, teamsThatKnowMaximumSubjects}
-        return output
+        return maximumSubjectsKnownByTeams, teamsThatKnowMaximumSubjects
     }
 
-        func CountSubjectsKnownBy2Teams(binaryString1 string, binaryString2 string) uint32 {
-            var subjectsKnownBy2Teams uint32 = 0
-
-            for i := 0; i < len(binaryString1); i++ {
+        func countSubjectsKnownBy2Teams(binaryString1 string, binaryString2 string) int {
+            var subjectsKnownBy2Teams int = 0
+            for i := range binaryString1 {
                 if binaryString1[i] == '1' || binaryString2[i] == '1' {
                     subjectsKnownBy2Teams += 1
                 }
             }
-
             return subjectsKnownBy2Teams
         }
 
-        func UpdateMaximumSubjectsAndTeamsThatKnowThem(subjectsKnownBy2Teams uint32, maximumSubjectsKnownByTeams *uint32, teamsThatKnowMaximumSubjects *uint32) {
+        func updateMaximumSubjectsAndTeamsThatKnowThem(subjectsKnownBy2Teams int, maximumSubjectsKnownByTeams *int, teamsThatKnowMaximumSubjects *int) {
             if subjectsKnownBy2Teams > *maximumSubjectsKnownByTeams {
                 *maximumSubjectsKnownByTeams = subjectsKnownBy2Teams
                 *teamsThatKnowMaximumSubjects = 1
@@ -64,9 +52,3 @@ func main() {
                 (*teamsThatKnowMaximumSubjects)++
             }
         }
-
-    func PrintArray(array [2]uint32) {
-        for _, element := range array {
-            fmt.Println(element)
-        }
-    }
--- hackerrank/problem solving/algorithms/2 - implementation/40 - acm icpc team/acm_icpc_team.rs
+++ hackerrank/problem solving/algorithms/2 - implementation/40 - acm icpc team/acm_icpc_team.rs
@@ -1,33 +1,33 @@
-// Source: https://www.hackerrank.com/challenges/acm-icpc-team/problem?isFullScreen=true
+// https://www.hackerrank.com/challenges/acm-icpc-team/problem?isFullScreen=true
 
-#[macro_use]
-extern crate text_io;
+use text_io::read;
 
 fn main() {
-    let attendees: u32 = read!();
-    let _topics: u32 = read!();
-    let binary_strings: Vec<String> = read_binary_strings(attendees);
-
-    let output: Vec<u32> = find_maximum_subjects_and_teams_that_know_them(binary_strings);
-    print_array(output);
+    let attendees: usize = read!();
+    let _topics: usize = read!();
+    let binary_strings: Vec<String> = read_binaries(attendees);
+    print_array(find_maximum_subjects_and_teams_that_know_them(
+        binary_strings,
+    ));
 }
 
-fn read_binary_strings(n_strings: u32) -> Vec<String> {
-    let mut binary_strings: Vec<String> = Vec::new();
-    binary_strings.resize_with(n_strings as usize, || read!());
-    return binary_strings;
+fn read_binaries(n: usize) -> Vec<String> {
+    let mut binaries: Vec<String> = Vec::new();
+    binaries.resize_with(n, || read!());
+    return binaries;
 }
 
-fn find_maximum_subjects_and_teams_that_know_them(binary_strings: Vec<String>) -> Vec<u32> {
-    let mut maximum_subjects_known_by_teams: u32 = 0;
-    let mut teams_that_know_maximum_subjects: u32 = 0;
+fn find_maximum_subjects_and_teams_that_know_them(binary_strings: Vec<String>) -> [i32; 2] {
+    let mut maximum_subjects_known_by_teams: i32 = 0;
+    let mut teams_that_know_maximum_subjects: i32 = 0;
 
-    for i in 0..(binary_strings.len() - 1) {
-        for j in (i + 1)..binary_strings.len() {
-            let subjects_known_by_2_teams: u32 = count_subjects_known_by_2_teams(
-                binary_strings[i].clone(),
-                binary_strings[j].clone(),
-            );
+    for (i, previous) in binary_strings[..binary_strings.len() - 1]
+        .iter()
+        .enumerate()
+    {
+        for current in &binary_strings[i + 1..] {
+            let subjects_known_by_2_teams: i32 = count_subjects_known_by_2_teams(previous, current);
+
             update_maximum_subjects_and_teams_that_know_them(
                 subjects_known_by_2_teams,
                 &mut maximum_subjects_known_by_teams,
@@ -36,31 +36,26 @@ fn find_maximum_subjects_and_teams_that_know_them(binary_strings: Vec<String>) -
         }
     }
 
-    let output: Vec<u32> = vec![
+    return [
         maximum_subjects_known_by_teams,
         teams_that_know_maximum_subjects,
     ];
-    return output;
 }
 
-fn count_subjects_known_by_2_teams(binary_string_1: String, binary_string_2: String) -> u32 {
-    let mut subjects_known_by_2_teams: u32 = 0;
-
-    for it in binary_string_1.chars().zip(binary_string_2.chars()) {
-        let (character_string_1, character_string_2) = it;
-
-        if character_string_1 == '1' || character_string_2 == '1' {
+fn count_subjects_known_by_2_teams(binary_string_1: &String, binary_string_2: &String) -> i32 {
+    let mut subjects_known_by_2_teams: i32 = 0;
+    for (ch1, ch2) in binary_string_1.chars().zip(binary_string_2.chars()) {
+        if ch1 == '1' || ch2 == '1' {
             subjects_known_by_2_teams += 1;
         }
     }
-
     return subjects_known_by_2_teams;
 }
 
 fn update_maximum_subjects_and_teams_that_know_them(
-    subjects_known_by_2_teams: u32,
-    maximum_subjects_known_by_teams: &mut u32,
-    teams_that_know_maximum_subjects: &mut u32,
+    subjects_known_by_2_teams: i32,
+    maximum_subjects_known_by_teams: &mut i32,
+    teams_that_know_maximum_subjects: &mut i32,
 ) {
     if subjects_known_by_2_teams > *maximum_subjects_known_by_teams {
         *maximum_subjects_known_by_teams = subjects_known_by_2_teams;
@@ -70,8 +65,8 @@ fn update_maximum_subjects_and_teams_that_know_them(
     }
 }
 
-fn print_array(array: Vec<u32>) {
-    for element in array {
-        println!("{}", element);
+fn print_array(array: [i32; 2]) {
+    for x in array {
+        println!("{}", x);
     }
 }
