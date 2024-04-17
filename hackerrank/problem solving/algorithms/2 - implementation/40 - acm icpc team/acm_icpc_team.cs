d113e3c48ade7b106bc1e4d35eadbeee1e7feb01
fernandozuher <fernando.zuher@gmail.com>
2024-04-17 14:12:23 -0300

    refactored

---
+++ hackerrank/problem solving/algorithms/2 - implementation/40 - acm icpc team/acm_icpc_team.cs
@@ -0,0 +1,80 @@
+// https://www.hackerrank.com/challenges/acm-icpc-team/problem?isFullScreen=true
+
+using static System.Console;
+using System.Numerics;
+
+namespace ProgrammingProblem
+{
+class Solution
+{
+    static void Main()
+    {
+        List<int>? array = ReadIntArray();
+        int attendees = array.First(), topics = array.Last();
+        List<BigInteger> binaries = ReadBinaries(attendees);
+        var obj = new ACM_ICPC_TEAM(binaries);
+        obj.FindMaximumSubjectsAndTeamsThatKnowThem();
+        WriteLine("{0}\n{1}", obj.MaximumSubjectsKnownByTeams, obj.TeamsThatKnowMaximumSubjects);
+    }
+
+        static List<int> ReadIntArray()
+        {
+            return ReadLine().Split().Select(int.Parse).ToList();
+        }
+
+        static List<BigInteger> ReadBinaries(int n)
+        {
+            return new List<BigInteger>(new BigInteger[n])
+                .Select(_ => ReadLine().Aggregate(new BigInteger(), (b, c) => b * 2 + c - '0'))
+                .ToList();
+        }
+}
+
+class ACM_ICPC_TEAM
+{
+    private List<BigInteger> _binaries;
+    private int _maximumSubjectsKnownByTeams;
+    private int _teamsThatKnowMaximumSubjects;
+
+    public ACM_ICPC_TEAM(List<BigInteger> binaries)
+    {
+        _binaries = binaries;
+        _maximumSubjectsKnownByTeams = 0;
+        _teamsThatKnowMaximumSubjects = 0;
+    }
+
+    public void FindMaximumSubjectsAndTeamsThatKnowThem()
+    {
+        int i = 0;
+        foreach (var previous in _binaries.SkipLast(1))
+            foreach (var current in _binaries.Skip(++i))
+                _updateMaximumSubjectsAndTeamsThatKnowThem(_countSubjectsKnownBy2Teams(previous, current));
+    }
+
+        private int _countSubjectsKnownBy2Teams(BigInteger binary1, BigInteger binary2)
+        {
+            return (int)BigInteger.PopCount(binary1 | binary2);
+        }
+
+        private void _updateMaximumSubjectsAndTeamsThatKnowThem(int subjectsKnownBy2Teams)
+        {
+            if (subjectsKnownBy2Teams > _maximumSubjectsKnownByTeams)
+            {
+                _maximumSubjectsKnownByTeams = subjectsKnownBy2Teams;
+                _teamsThatKnowMaximumSubjects = 1;
+            }
+            else if (subjectsKnownBy2Teams == _maximumSubjectsKnownByTeams)
+                _teamsThatKnowMaximumSubjects += 1;
+        }
+
+    public int MaximumSubjectsKnownByTeams
+    {
+        get { return _maximumSubjectsKnownByTeams; }
+    }
+
+    public int TeamsThatKnowMaximumSubjects
+    {
+        get { return _teamsThatKnowMaximumSubjects; }
+    }
+}
+}
