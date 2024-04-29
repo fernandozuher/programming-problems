0aa91e09c93478c18e1d2324723404341bcde749
fernandozuher <fernando.zuher@gmail.com>
2024-04-29 15:09:40 -0300

    refactored

--- hackerrank/problem solving/algorithms/2 - implementation/45 - modified kaprekar numbers/modified_kaprekar_numbers.c
+++ hackerrank/problem solving/algorithms/2 - implementation/45 - modified kaprekar numbers/modified_kaprekar_numbers.c
@@ -1,4 +1,5 @@
 // https://www.hackerrank.com/challenges/kaprekar-numbers/problem?isFullScreen=true
+// Written in C23: false and true first-class language features
 
 #include <math.h>
 #include <stdio.h>
@@ -11,54 +12,49 @@ typedef struct {
     int arr[MAX_QUANTITY_OF_KAPREKAR_NUMBERS];
 } array;
 
-array find_kaprekar_numbers_in_range(int lower_limit, int upper_limit);
-    int calculate_potential_kaprekar_number(int number);
-        int number_digits(long n);
+bool is_number_kaprekar(int number);
+    int number_digits(long n);
 void print_output(const array* const arr);
 
 int main()
 {
     int lower_limit, upper_limit;
     scanf("%d %d", &lower_limit, &upper_limit);
-    array kaprekar_numbers_in_range = find_kaprekar_numbers_in_range(lower_limit, upper_limit);
-    print_output(&kaprekar_numbers_in_range);
+
+    array kaprekar_numbers = {0, {0}};
+    for (int number = lower_limit; number <= upper_limit; ++number) {
+        if (is_number_kaprekar(number))
+            kaprekar_numbers.arr[kaprekar_numbers.size++] = number;
+    }
+
+    print_output(&kaprekar_numbers);
 
     return 0;
 }
 
-    array find_kaprekar_numbers_in_range(const int lower_limit, const int upper_limit)
+    bool is_number_kaprekar(const int number)
     {
-        array kaprekar_numbers = {0, 0};
-        for (int number = lower_limit; number <= upper_limit; ++number) {
-            if (number == calculate_potential_kaprekar_number(number))
-                kaprekar_numbers.arr[kaprekar_numbers.size++] = number;
-        }
-        return kaprekar_numbers;
+        long square_number = powl(number, 2);
+        long divisor = powl(10, number_digits(number));
+
+        ldiv_t x = ldiv(square_number, divisor);
+        int left_number = x.quot;
+        int right_number = x.rem;
+
+        return number == left_number + right_number;
     }
 
-        int calculate_potential_kaprekar_number(const int number)
+        int number_digits(const long n)
         {
-            long square_number = powl(number, 2);
-            long divisor = powl(10, number_digits(number));
-
-            ldiv_t x = ldiv(square_number, divisor);
-            int left_number = x.quot;
-            int right_number = x.rem;
-
-            return left_number + right_number;
+            if (n < 10) return 1;
+            if (n < 100) return 2;
+            if (n < 1000) return 3;
+            if (n < 10000) return 4;
+            if (n < 100000) return 5;
+            if (n < 1000000) return 6;
+            if (n < 10000000) return 7;
+            return 8;
         }
-
-            int number_digits(const long n)
-            {
-                if (n < 10) return 1;
-                if (n < 100) return 2;
-                if (n < 1000) return 3;
-                if (n < 10000) return 4;
-                if (n < 100000) return 5;
-                if (n < 1000000) return 6;
-                if (n < 10000000) return 7;
-                return 8;
-            }
 
     void print_output(const array* const arr)
     {
--- hackerrank/problem solving/algorithms/2 - implementation/45 - modified kaprekar numbers/modified_kaprekar_numbers.cpp
+++ hackerrank/problem solving/algorithms/2 - implementation/45 - modified kaprekar numbers/modified_kaprekar_numbers.cpp
@@ -7,72 +7,42 @@
 #include <vector>
 
 using namespace std;
+using namespace views;
 
-namespace programing_problems {
-
-int calculate_potential_kaprekar_number(int number);
+bool is_number_kaprekar(int number);
     int number_digits(long n);
-
-class Kaprekar_Numbers {
-    int lower_limit, upper_limit;
-    vector<int> vec_kaprekar_numbers;
-
-public:
-    Kaprekar_Numbers(int lower_limit, int upper_limit);
-    [[nodiscard]] vector<int> kaprekar_numbers() const;
-    void find_kaprekar_numbers_in_range();
-};
-
-    Kaprekar_Numbers::Kaprekar_Numbers(const int lower_limit, const int upper_limit):
-        lower_limit{lower_limit}, upper_limit{upper_limit} {}
-
-    [[nodiscard]] vector<int> Kaprekar_Numbers::kaprekar_numbers() const
-    {
-        return vec_kaprekar_numbers;
-    }
-
-    void Kaprekar_Numbers::find_kaprekar_numbers_in_range()
-    {
-        for (const int number : views::iota(lower_limit, upper_limit + 1))
-            if (number == calculate_potential_kaprekar_number(number))
-                vec_kaprekar_numbers.push_back(number);
-    }
-
-        int calculate_potential_kaprekar_number(const int number)
-        {
-            auto square_number {pow(number, 2)};
-            auto divisor {pow(10, number_digits(number))};
-            auto [left_number, right_number] {ldiv(square_number, divisor)};
-            return left_number + right_number;
-        }
-
-            int number_digits(const long n)
-            {
-                if (n < 10) return 1;
-                if (n < 100) return 2;
-                if (n < 1000) return 3;
-                if (n < 10000) return 4;
-                if (n < 100000) return 5;
-                if (n < 1000000) return 6;
-                if (n < 10000000) return 7;
-                return 8;
-            }
-}
-
-//////////////////////////////////////////////////
-
 void print_output(const vector<int>& array);
 
 int main()
 {
     int lower_limit, upper_limit;
     cin >> lower_limit >> upper_limit;
-    programing_problems::Kaprekar_Numbers obj{lower_limit, upper_limit};
-    obj.find_kaprekar_numbers_in_range();
-    print_output(obj.kaprekar_numbers());
+    vector<int> kaprekar_numbers;
+    ranges::copy_if(iota(lower_limit, upper_limit + 1), back_inserter(kaprekar_numbers), is_number_kaprekar);
+    print_output(kaprekar_numbers);
 
     return 0;
 }
+
+    bool is_number_kaprekar(const int number)
+    {
+        auto square_number {pow(number, 2)};
+        auto divisor {pow(10, number_digits(number))};
+        auto [left_number, right_number] {ldiv(square_number, divisor)};
+        return number == left_number + right_number;
+    }
+
+        int number_digits(const long n)
+        {
+            if (n < 10) return 1;
+            if (n < 100) return 2;
+            if (n < 1000) return 3;
+            if (n < 10000) return 4;
+            if (n < 100000) return 5;
+            if (n < 1000000) return 6;
+            if (n < 10000000) return 7;
+            return 8;
+        }
 
     void print_output(const vector<int>& array)
     {
