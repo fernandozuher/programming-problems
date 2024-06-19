b4785f46d3fa303445b94989db2f4df75da3fc69
Fernando Zuher <fernando.zuer@gmail.com>
2024-06-19 18:34:48 -0300

    refactored

--- hackerrank/problem solving/algorithms/2 - implementation/2 - apple and orange/apple_and_orange.c
+++ hackerrank/problem solving/algorithms/2 - implementation/2 - apple and orange/apple_and_orange.c
@@ -1,4 +1,5 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
+// From C23
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,10 +14,10 @@ typedef struct {
 } apple_and_orange;
 
 apple_and_orange read_input();
-    int* read_int_array(const int n);
-void count_apples_and_oranges(const apple_and_orange const *input);
-    int count_fruits_on_house(const apple_and_orange const *input, const char const *fruit);
-        int** filter_input(const apple_and_orange const *input, const char const *fruit);
+    int *read_int_array(const int n);
+void count_apples_and_oranges(const apple_and_orange *input);
+    int count_fruits_on_house(const apple_and_orange *input, const char *fruit);
+        int **filter_input(const apple_and_orange *input, const char *fruit);
 
 int main()
 {
@@ -28,31 +29,30 @@ int main()
     apple_and_orange read_input()
     {
         apple_and_orange input;
-
         scanf("%d %d", &input.starting_sam, &input.ending_sam);
         scanf("%d %d", &input.apple_tree_location, &input.orange_tree_location);
         scanf("%d %d", &input.n_apples, &input.n_oranges);
         input.apples_distance_from_tree = read_int_array(input.n_apples);
         input.oranges_distance_from_tree = read_int_array(input.n_oranges);
-
         return input;
     }
 
-        int* read_int_array(const int n)
+        int *read_int_array(const int n)
         {
-            int *array = (int*) calloc(n, sizeof(int));
-            for (int i = 0; i < n; scanf("%d", &array[i++]));
+            auto array = (int*) malloc(n * sizeof(int));
+            for (int i = 0; i < n; ++i)
+                scanf("%d", &array[i]);
             return array;
         }
 
-    void count_apples_and_oranges(const apple_and_orange const *input)
+    void count_apples_and_oranges(const apple_and_orange *const input)
     {
         int apples_on_house = count_fruits_on_house(input, "apple");
         int oranges_on_house = count_fruits_on_house(input, "orange");
         printf("%d\n%d", apples_on_house, oranges_on_house);
     }
 
-        int count_fruits_on_house(const apple_and_orange const *input, const char const *fruit)
+        int count_fruits_on_house(const apple_and_orange *const input, const char *const fruit)
         {
             int **filtered_input = filter_input(input, fruit);
             int tree_location = *filtered_input[0];
@@ -69,10 +69,10 @@ int main()
             return fruits_on_house;
         }
 
-            int** filter_input(const apple_and_orange const *input, const char const *fruit)
+            int **filter_input(const apple_and_orange *const input, const char *const fruit)
             {
-                const int data = 3;
-                int **filtered_input = (int**) calloc(data, sizeof(int*));
+                constexpr int data = 3;
+                auto filtered_input = (int**) malloc(data * sizeof(int*));
                 for (int i = 0; i < data - 1; ++i)
                     filtered_input[i] = (int*) malloc(sizeof(int));
 
--- hackerrank/problem solving/algorithms/2 - implementation/2 - apple and orange/apple_and_orange.cpp
+++ hackerrank/problem solving/algorithms/2 - implementation/2 - apple and orange/apple_and_orange.cpp
@@ -2,23 +2,24 @@
 
 #include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <vector>
 
 using namespace std;
 
 typedef struct {
-    int starting_sam, ending_sam;
-    int apple_tree_location, orange_tree_location;
-    int n_apples, n_oranges;
+    int starting_sam{}, ending_sam{};
+    int apple_tree_location{}, orange_tree_location{};
+    int n_apples{}, n_oranges{};
     vector<int> apples_distance_from_tree;
     vector<int> oranges_distance_from_tree;
 } apple_and_orange;
 
 apple_and_orange read_input();
-    vector<int> read_int_array(const int n);
+    vector<int> read_int_array(int n);
 void count_apples_and_oranges(const apple_and_orange& input);
-    int count_fruits_on_house(const apple_and_orange& input, const string& fruit);
-        vector<vector<int>> filter_input(const apple_and_orange& input, const string& fruit);
+    int count_fruits_on_house(const apple_and_orange& input, string_view fruit);
+        tuple<int, vector<int>> filter_input(const apple_and_orange& input, string_view fruit);
 
 int main()
 {
@@ -30,20 +31,18 @@ int main()
     apple_and_orange read_input()
     {
         apple_and_orange input;
-
         cin >> input.starting_sam >> input.ending_sam;
         cin >> input.apple_tree_location >> input.orange_tree_location;
         cin >> input.n_apples >> input.n_oranges;
         input.apples_distance_from_tree = read_int_array(input.n_apples);
         input.oranges_distance_from_tree = read_int_array(input.n_oranges);
-
         return input;
     }
 
         vector<int> read_int_array(const int n)
         {
             vector<int> array(n);
-            ranges::generate(array, [] {int n; cin >> n; return n;});
+            copy_n(istream_iterator<int>(cin), n, array.begin());
             return array;
         }
 
@@ -54,33 +53,21 @@ int main()
         cout << apples_on_house << '\n' << oranges_on_house;
     }
 
-        int count_fruits_on_house(const apple_and_orange& input, const string& fruit)
+        int count_fruits_on_house(const apple_and_orange& input, const string_view fruit)
         {
-            vector<vector<int>> filtered_input {filter_input(input, fruit)};
-            int tree_location {filtered_input.front().front()};
-            vector<int> fruits {filtered_input.back()};
+            const auto [tree_location, fruits] {filter_input(input, fruit)};
 
             auto verify_fruit_location = [&] (const int partial_location) {
                 int location {tree_location + partial_location};
                 return location >= input.starting_sam && location <= input.ending_sam;
             };
 
-            return ranges::count_if(fruits, verify_fruit_location);
+            return static_cast<int>(ranges::count_if(fruits, verify_fruit_location));
         }
 
-            vector<vector<int>> filter_input(const apple_and_orange& input, const string& fruit)
+            tuple<int, vector<int>> filter_input(const apple_and_orange& input, const string_view fruit)
             {
-                constexpr int data {2};
-                vector<vector<int>> filtered_input(data);
-
-                if (fruit == "apple") {
-                    filtered_input.front().push_back(input.apple_tree_location);
-                    filtered_input.back() = input.apples_distance_from_tree;
-                }
-                else {
-                    filtered_input.front().push_back(input.orange_tree_location);
-                    filtered_input.back() = input.oranges_distance_from_tree;
-                }
-
-                return filtered_input;
+                if (fruit == "apple")
+                    return {input.apple_tree_location, input.apples_distance_from_tree};
+                return {input.orange_tree_location, input.oranges_distance_from_tree};
             }
