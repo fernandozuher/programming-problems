a92e9f4972c36d0c698584a39e9c3c056693c6b7
Fernando Zuher <fernando.zuher@gmail.com>
2024-09-03 15:21:51 -0300

    12.8 - copy node

---
+++ cracking the code interview/IX - interview questions/ch 12 - c and cpp/8.cpp
@@ -0,0 +1,54 @@
+// From C++23
+
+#include <iostream>
+#include <print>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* a;
+    Node* b;
+};
+
+Node* copy_node(const Node* node);
+void print_node(const Node* node);
+
+int main()
+{
+    auto node_a = new Node{1};
+    node_a->a = new Node{2};
+    node_a->b = new Node{3};
+    cout << "Node a: ";
+    print_node(node_a);
+
+    auto node_b{copy_node(node_a)};
+    cout << "Node b: ";
+    print_node(node_b);
+
+    node_a->data = 11;
+    node_a->a->data = 22;
+    node_a->b->data = 33;
+    cout << "\nNode a: ";
+    print_node(node_a);
+
+    cout << "Node b: ";
+    print_node(node_b);
+
+    return 0;
+}
+
+Node* copy_node(const Node* node)
+{
+    auto new_node = new Node{*node};
+    if (node->a)
+        new_node->a = copy_node(node->a);
+    if (node->b)
+        new_node->b = copy_node(node->b);
+    return new_node;
+}
+
+void print_node(const Node* node)
+{
+    println("{}: {}, {}", node->data, node->a->data, node->b->data);
+}
