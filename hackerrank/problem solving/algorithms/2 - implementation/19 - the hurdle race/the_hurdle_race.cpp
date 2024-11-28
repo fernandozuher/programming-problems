d8ceaaa1311bfec13bb2d17a5f3137ff4033bddb
Fernando Zuher <fernando.zuher@gmail.com>
2024-11-28 12:27:20 -0300

    refactored

--- hackerrank/problem solving/algorithms/2 - implementation/19 - the hurdle race/the_hurdle_race.cpp
+++ hackerrank/problem solving/algorithms/2 - implementation/19 - the hurdle race/the_hurdle_race.cpp
@@ -1,64 +1,34 @@
 // https://www.hackerrank.com/challenges/the-hurdle-race/problem?isFullScreen=true
+// From C++20 onwards
 
 #include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <vector>
 
 using namespace std;
 
-template<class T = int>
-vector<T> read_array(const int n);
-
-class Hurdle_Race {
-public:
-    Hurdle_Race(const vector<int>& hurdles_heights, const int maximum_height_can_jump);
-    int doses() const;
-
-private:
-    vector<int> hurdles_heights;
-    int maximum_height_can_jump;
-    int n_doses;
-
-    void hurdle_race();
-};
-
-    Hurdle_Race::Hurdle_Race(const vector<int>& hurdles_heights, const int maximum_height_can_jump)
-        : hurdles_heights{hurdles_heights},
-          maximum_height_can_jump{maximum_height_can_jump},
-          n_doses{}
-    {
-        hurdle_race();
-    }
-
-        void Hurdle_Race::hurdle_race()
-        {
-            int highest_hurdle {*ranges::max_element(hurdles_heights)};
-            n_doses = highest_hurdle > maximum_height_can_jump ? highest_hurdle - maximum_height_can_jump : 0;
-        }
-
-    int Hurdle_Race::doses() const
-    {
-        return n_doses;
-    }
-
-/////////////////////////////////////////////////
+template <class T = int>
+vector<T> read(int n);
+int hurdle_race(const vector<int>& hurdles_heights, int maximum_height_can_jump);
 
 int main()
 {
     int n, maximum_height_can_jump;
     cin >> n >> maximum_height_can_jump;
-    vector<int> hurdles_heights {read_array(n)};
-
-    Hurdle_Race obj{hurdles_heights, maximum_height_can_jump};
-    cout << obj.doses();
-
+    cout << hurdle_race(read(n), maximum_height_can_jump);
     return 0;
 }
 
-    template<class T = int>
-    vector<T> read_array(const int n)
-    {
-        vector<T> array(n);
-        ranges::generate(array, [] {T x; cin >> x; return x;});
-        return array;
-    }
+template <class T>
+vector<T> read(const int n)
+{
+    vector<T> array(n);
+    copy_n(istream_iterator<T>(cin), n, array.begin());
+    return array;
+}
+
+int hurdle_race(const vector<int>& hurdles_heights, const int maximum_height_can_jump)
+{
+    return max(*ranges::max_element(hurdles_heights) - maximum_height_can_jump, 0);
+}
