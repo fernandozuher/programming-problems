2583f0204eee5842fe05385248d28274ffa25258
Fernando <fernando.zuher@gmail.com>
2025-02-23 20:33:23 -0300

    refactored

--- hackerrank/30 days of code/day 10, binary numbers/binary_numbers.c
+++ hackerrank/30 days of code/day 10, binary numbers/binary_numbers.c
@@ -1,24 +1,61 @@
 // https://www.hackerrank.com/challenges/30-binary-numbers/problem?isFullScreen=true
+// From C23 onwards: constexpr, bool as built-in type, nullptr, auto
 
 #include <stdio.h>
+#include <stdlib.h>
+
+constexpr int N_BITS_OF_BINARY_INT = sizeof(int) * 8;
+
+int count_max_consecutive_ones_in_binary_from(int n);
+bool *int_to_binary(int n);
+int max_consecutive_ones_from(const bool *binary);
+bool is_least_significant_bit_on(int n);
+int max(int a, int b);
 
 int main()
 {
     int n;
     scanf("%d", &n);
-
-    int max_ones = 0, ones = 0;
-    for (; n > 0; n /= 2)
-        if (n & 1)
-            ++ones;
-        else {
-            if (ones > max_ones)
-                max_ones = ones;
-            ones = 0;
-        }
-
-    max_ones = max_ones > ones ? max_ones : ones;
-    printf("%d\n", max_ones);
-
+    printf("%d\n", count_max_consecutive_ones_in_binary_from(n));
     return 0;
+}
+
+int count_max_consecutive_ones_in_binary_from(const int n)
+{
+    bool *binary = int_to_binary(n);
+    int max_consecutive_ones = max_consecutive_ones_from(binary);
+    free(binary);
+    binary = nullptr;
+    return max_consecutive_ones;
+}
+
+bool *int_to_binary(int n)
+{
+    auto binary = (bool *) calloc(N_BITS_OF_BINARY_INT, sizeof(bool));
+    for (int i = 0; i < N_BITS_OF_BINARY_INT && n > 0; ++i, n /= 2)
+        binary[i] = is_least_significant_bit_on(n);
+    return binary;
+}
+
+bool is_least_significant_bit_on(const int n)
+{
+    return n & 1;
+}
+
+int max_consecutive_ones_from(const bool *const binary)
+{
+    int consecutive_ones = 0, max_consecutive_ones = 0;
+    for (int i = 0; i < N_BITS_OF_BINARY_INT; ++i)
+        if (binary[i])
+            ++consecutive_ones;
+        else {
+            max_consecutive_ones = max(consecutive_ones, max_consecutive_ones);
+            consecutive_ones = 0;
+        }
+    return max(consecutive_ones, max_consecutive_ones);
+}
+
+int max(const int a, const int b)
+{
+    return a >= b ? a : b;
 }
--- hackerrank/30 days of code/day 10, binary numbers/binary_numbers.cpp
+++ hackerrank/30 days of code/day 10, binary numbers/binary_numbers.cpp
@@ -1,25 +1,42 @@
 // https://www.hackerrank.com/challenges/30-binary-numbers/problem?isFullScreen=true
 
+#include <bitset>
 #include <iostream>
 
 using namespace std;
+
+int count_max_consecutive_ones_in_binary_from(int n);
+bitset<32> int_to_binary(int n);
+int max_consecutive_ones_from(const bitset<32> &binary);
 
 int main()
 {
     int n;
     cin >> n;
-
-    int max_ones{}, ones{};
-    for (; n > 0; n /= 2)
-        if (n & 1)
-            ++ones;
-        else {
-            max_ones = max(max_ones, ones);
-            ones = 0;
-        }
-
-    max_ones = max(max_ones, ones);
-    cout << max_ones;
-
+    cout << count_max_consecutive_ones_in_binary_from(n);
     return 0;
+}
+
+int count_max_consecutive_ones_in_binary_from(const int n)
+{
+    bitset binary{int_to_binary(n)};
+    return max_consecutive_ones_from(binary);
+}
+
+bitset<32> int_to_binary(const int n)
+{
+    return n;
+}
+
+int max_consecutive_ones_from(const bitset<32> &binary)
+{
+    int consecutive_ones{}, max_consecutive_ones{};
+    for (int i{}; i < binary.size(); ++i)
+        if (binary[i])
+            ++consecutive_ones;
+        else {
+            max_consecutive_ones = max(consecutive_ones, max_consecutive_ones);
+            consecutive_ones = 0;
+        }
+    return max(consecutive_ones, max_consecutive_ones);
 }
