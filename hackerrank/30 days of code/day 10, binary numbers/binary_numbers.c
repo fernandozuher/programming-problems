304590bbbf456994d7ba003b7f7aa3bd55d01f05
Fernando <fernando.zuher@gmail.com>
2025-02-24 22:55:06 -0300

    refactored

--- hackerrank/30 days of code/day 10, binary numbers/binary_numbers.c
+++ hackerrank/30 days of code/day 10, binary numbers/binary_numbers.c
@@ -6,27 +6,22 @@
 
 constexpr int N_BITS_OF_BINARY_INT = sizeof(int) * 8;
 
-int count_max_consecutive_ones_in_binary_from(int n);
 bool *int_to_binary(int n);
-int max_consecutive_ones_from(const bool *binary);
 bool is_least_significant_bit_on(int n);
+int max_consecutive_ones_from(const bool *binary);
+int *count_ones_in_sequence(int i, const bool *binary);
+int find_last_index_with_consecutive_ones(int i, const bool *binary);
 int max(int a, int b);
 
 int main()
 {
     int n;
     scanf("%d", &n);
-    printf("%d\n", count_max_consecutive_ones_in_binary_from(n));
-    return 0;
-}
+    bool *binary = int_to_binary(n); // lest significant bit starting from the left
+    printf("%d\n", max_consecutive_ones_from(binary));
 
-int count_max_consecutive_ones_in_binary_from(const int n)
-{
-    bool *binary = int_to_binary(n);
-    int max_consecutive_ones = max_consecutive_ones_from(binary);
     free(binary);
-    binary = nullptr;
-    return max_consecutive_ones;
+    return 0;
 }
 
 bool *int_to_binary(int n)
@@ -44,15 +39,42 @@ bool is_least_significant_bit_on(const int n)
 
 int max_consecutive_ones_from(const bool *const binary)
 {
-    int consecutive_ones = 0, max_consecutive_ones = 0;
-    for (int i = 0; i < N_BITS_OF_BINARY_INT; ++i)
-        if (binary[i])
-            ++consecutive_ones;
-        else {
-            max_consecutive_ones = max(consecutive_ones, max_consecutive_ones);
-            consecutive_ones = 0;
+    if (binary == nullptr)
+        return 0;
+
+    int max_consecutive_ones = 0;
+    for (int i = 0; i < N_BITS_OF_BINARY_INT; ++i) {
+        bool is_bit_set = binary[i];
+
+        if (is_bit_set) {
+            int *i_and_consecutive_ones = count_ones_in_sequence(i, binary);
+            i = i_and_consecutive_ones[0];
+            max_consecutive_ones = max(i_and_consecutive_ones[1], max_consecutive_ones);
+            free(i_and_consecutive_ones);
         }
-    return max(consecutive_ones, max_consecutive_ones);
+    }
+    return max_consecutive_ones;
+}
+
+int *count_ones_in_sequence(const int i, const bool *const binary)
+{
+    int first_index = i;
+    int last_index = find_last_index_with_consecutive_ones(i, binary);
+    int consecutive_ones = last_index - first_index + 1;
+
+    auto output = (int *) malloc(2 * sizeof(int));
+    output[0] = last_index;
+    output[1] = consecutive_ones;
+    return output;
+}
+
+int find_last_index_with_consecutive_ones(int i, const bool *const binary)
+{
+    if (binary == nullptr)
+        return 0;
+    ++i;
+    for (; i < N_BITS_OF_BINARY_INT && binary[i]; ++i);
+    return i - 1;
 }
 
 int max(const int a, const int b)
--- hackerrank/30 days of code/day 10, binary numbers/binary_numbers.cpp
+++ hackerrank/30 days of code/day 10, binary numbers/binary_numbers.cpp
@@ -2,41 +2,45 @@
 
 #include <bitset>
 #include <iostream>
+#include <tuple>
 
 using namespace std;
 
-int count_max_consecutive_ones_in_binary_from(int n);
-bitset<32> int_to_binary(int n);
-int max_consecutive_ones_from(const bitset<32> &binary);
+int max_consecutive_ones_from(const bitset<32>& binary);
+tuple<int, int> count_ones_in_sequence(int i, const bitset<32>& binary);
+int find_last_index_with_consecutive_ones(int i, const bitset<32>& binary);
 
 int main()
 {
     int n;
     cin >> n;
-    cout << count_max_consecutive_ones_in_binary_from(n);
+    auto binary{bitset<32>(n)};
+    cout << max_consecutive_ones_from(binary);
     return 0;
 }
 
-int count_max_consecutive_ones_in_binary_from(const int n)
+int max_consecutive_ones_from(const bitset<32>& binary)
 {
-    bitset binary{int_to_binary(n)};
-    return max_consecutive_ones_from(binary);
-}
-
-bitset<32> int_to_binary(const int n)
-{
-    return n;
-}
-
-int max_consecutive_ones_from(const bitset<32> &binary)
-{
-    int consecutive_ones{}, max_consecutive_ones{};
-    for (int i{}; i < binary.size(); ++i)
-        if (binary[i])
-            ++consecutive_ones;
-        else {
+    int max_consecutive_ones{};
+    for (int i{}, consecutive_ones{}; i < binary.size(); ++i)
+        if (const bool is_bit_on{binary[i]}; is_bit_on) {
+            tie(i, consecutive_ones) = count_ones_in_sequence(i, binary);
             max_consecutive_ones = max(consecutive_ones, max_consecutive_ones);
-            consecutive_ones = 0;
         }
-    return max(consecutive_ones, max_consecutive_ones);
+    return max_consecutive_ones;
+}
+
+tuple<int, int> count_ones_in_sequence(int i, const bitset<32>& binary)
+{
+    int first_index{i};
+    int last_index{find_last_index_with_consecutive_ones(i, binary)};
+    int consecutive_ones{last_index - first_index + 1};
+    return {last_index, consecutive_ones};
+}
+
+int find_last_index_with_consecutive_ones(int i, const bitset<32>& binary)
+{
+    ++i;
+    for (; i < binary.size() && binary[i]; ++i);
+    return i - 1;
 }
