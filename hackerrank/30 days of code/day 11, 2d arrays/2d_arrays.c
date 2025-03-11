ae12f4585774f119074254b066eb65742a4ef593
Fernando <fernando.zuher@gmail.com>
2025-03-11 18:30:48 -0300

    Refactored

--- hackerrank/30 days of code/day 11, 2d arrays/2d_arrays.c
+++ hackerrank/30 days of code/day 11, 2d arrays/2d_arrays.c
@@ -1,4 +1,5 @@
 // https://www.hackerrank.com/challenges/30-2d-arrays/problem?isFullScreen=true
+// From C23 onward: auto (changed meaning), nullptr
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,62 +7,58 @@
 
 #define N_ROWS_COLUMNS 6
 
-int** read_matrix();
-int calculate_max_sum(int **matrix);
-    int sum_around(int **matrix, const int i, const int j);
-int** free_matrix(int **matrix);
+int **read_matrix();
+int calculate_max_sum(int **const matrix);
+int sum_around(int **const matrix, int i, int j);
+int max(int a, int b);
+int **free_matrix(int **matrix);
 
 int main()
 {
     int **matrix = read_matrix();
-    int max_sum = calculate_max_sum(matrix);
-
-    printf("%d\n", max_sum);
+    printf("%d\n", calculate_max_sum(matrix));
     matrix = free_matrix(matrix);
-
     return 0;
 }
 
-    int** read_matrix()
-    {
-        int **matrix = calloc(N_ROWS_COLUMNS, sizeof(int*));
+int **read_matrix()
+{
+    auto matrix = (int **) malloc(N_ROWS_COLUMNS * sizeof(int *));
 
-        for (int i = 0; i < N_ROWS_COLUMNS; ++i) {
-            matrix[i] = calloc(N_ROWS_COLUMNS, sizeof(int));
-
-            for (int j = 0; j < N_ROWS_COLUMNS; ++j)
-                scanf("%d", &matrix[i][j]);
-        }
-
-        return matrix;
+    for (int i = 0; i < N_ROWS_COLUMNS; ++i) {
+        matrix[i] = (int *) malloc(N_ROWS_COLUMNS * sizeof(int));
+        for (int j = 0; j < N_ROWS_COLUMNS; ++j)
+            scanf("%d", &matrix[i][j]);
     }
 
-    int calculate_max_sum(int **matrix)
-    {
-        int max_sum = INT_MIN;
+    return matrix;
+}
 
-        for (int i = 1, before_rows_columns_limit = 5; i < before_rows_columns_limit; ++i)
-            for (int j = 1; j < before_rows_columns_limit; ++j) {
-                int current_sum = sum_around(matrix, i, j);
+int calculate_max_sum(int **const matrix)
+{
+    int max_sum = INT_MIN;
+    for (int i = 1, before_rows_columns_limit = 5; i < before_rows_columns_limit; ++i)
+        for (int j = 1; j < before_rows_columns_limit; ++j)
+            max_sum = max(max_sum, sum_around(matrix, i, j));
+    return max_sum;
+}
 
-                if (current_sum > max_sum)
-                    max_sum = current_sum;
-            }
+int sum_around(int **const matrix, int i, int j)
+{
+    return matrix[i - 1][j - 1] + matrix[i - 1][j] + matrix[i - 1][j + 1]
+           + matrix[i][j]
+           + matrix[i + 1][j - 1] + matrix[i + 1][j] + matrix[i + 1][j + 1];
+}
 
-        return max_sum;
-    }
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
 
-        int sum_around(int **matrix, const int i, const int j)
-        {
-            return matrix[i - 1][j - 1] + matrix[i - 1][j] + matrix[i - 1][j + 1]
-                   + matrix[i][j]
-                   + matrix[i + 1][j - 1] + matrix[i + 1][j] + matrix[i + 1][j + 1];
-        }
-
-    int** free_matrix(int **matrix)
-    {
-        for (int i = 0; i < N_ROWS_COLUMNS; ++i)
-            free(matrix[i]);
-        free(matrix);
-        return NULL;
-    }
+int **free_matrix(int **matrix)
+{
+    for (int i = 0; i < N_ROWS_COLUMNS; ++i)
+        free(matrix[i]);
+    free(matrix);
+    return nullptr;
+}
--- hackerrank/30 days of code/day 11, 2d arrays/2d_arrays.cpp
+++ hackerrank/30 days of code/day 11, 2d arrays/2d_arrays.cpp
@@ -1,50 +1,46 @@
 // https://www.hackerrank.com/challenges/30-2d-arrays/problem?isFullScreen=true
+// From C++20 onward: ranges::
 
 #include <algorithm>
 #include <iostream>
-#include <climits>
 #include <vector>
 
 using namespace std;
 
 vector<vector<int>> read_matrix();
 int calculate_max_sum(const vector<vector<int>>& matrix);
-    int sum_around(const vector<vector<int>>& matrix, const int i, const int j);
+int sum_around(const vector<vector<int>>& matrix, int i, int j);
 
 int main()
 {
-    vector<vector<int>> matrix {read_matrix()};
-    int max_sum {calculate_max_sum(matrix)};
-    cout << max_sum;
-
+    vector matrix{read_matrix()};
+    cout << calculate_max_sum(matrix);
     return 0;
 }
 
-    vector<vector<int>> read_matrix()
-    {
-        constexpr int n_rows_columns {6};
-        vector<vector<int>> matrix(n_rows_columns, vector<int>(n_rows_columns));
+vector<vector<int>> read_matrix()
+{
+    constexpr int n_rows_columns{6};
+    vector<vector<int>> matrix(n_rows_columns, vector<int>(n_rows_columns));
+    for (auto &row: matrix)
+        ranges::generate(row, [] {int n; cin >> n; return n;});
+    return matrix;
+}
 
-        for (auto& row : matrix)
-            ranges::generate(row, [] {int n; cin >> n; return n;});
+int calculate_max_sum(const vector<vector<int>>& matrix)
+{
+    int max_sum{numeric_limits<int>::min()};
 
-        return matrix;
-    }
+    for (int i{1}, before_rows_columns_limit{5}; i < before_rows_columns_limit; ++i)
+        for (int j{1}; j < before_rows_columns_limit; ++j)
+            max_sum = max(max_sum, sum_around(matrix, i, j));
 
-    int calculate_max_sum(const vector<vector<int>>& matrix)
-    {
-        int max_sum {INT_MIN};
+    return max_sum;
+}
 
-        for (int i {1}, before_rows_columns_limit {5}; i < before_rows_columns_limit; ++i)
-            for (int j {1}; j < before_rows_columns_limit; ++j)
-                max_sum = max(max_sum, sum_around(matrix, i, j));
-
-        return max_sum;
-    }
-
-        int sum_around(const vector<vector<int>>& matrix, const int i, const int j)
-        {
-            return matrix.at(i - 1).at(j - 1) + matrix.at(i - 1).at(j) + matrix.at(i - 1).at(j + 1)
-                   + matrix.at(i).at(j)
-                   + matrix.at(i + 1).at(j - 1) + matrix.at(i + 1).at(j) + matrix.at(i + 1).at(j + 1);
-        }
+int sum_around(const vector<vector<int>>& matrix, int i, int j)
+{
+    return matrix.at(i - 1).at(j - 1) + matrix.at(i - 1).at(j) + matrix.at(i - 1).at(j + 1)
+           + matrix.at(i).at(j)
+           + matrix.at(i + 1).at(j - 1) + matrix.at(i + 1).at(j) + matrix.at(i + 1).at(j + 1);
+}
