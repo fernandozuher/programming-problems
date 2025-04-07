80800a83fc5db8b0e3d21cc861ca7f6be89fb98d
Fernando <fernando.zuher@gmail.com>
2025-04-07 20:17:51 -0300

    refactored

--- hackerrank/30 days of code/day 22, binary search trees/binary_search_trees.cpp
+++ hackerrank/30 days of code/day 22, binary search trees/binary_search_trees.cpp
@@ -1,74 +1,74 @@
 // https://www.hackerrank.com/challenges/30-binary-search-trees/problem?isFullScreen=true
 
 #include <iostream>
-
+#include <memory>
 using namespace std;
 
-class Node {
-public:
-    int data;
-    Node* left;
-    Node* right;
+typedef struct Node {
+    int data{};
+    shared_ptr<Node> left, right;
+    explicit Node(int d): data{d} {}
+} Node;
 
-    Node(int d)
-    {
-        data = d;
-        left = right = nullptr;
-    }
-};
-
-class Solution {
-public:
-    Node* insert(Node* root, int data)
-    {
-        if (!root)
-            return new Node(data);
-        else {
-            Node* cur;
-
-            if (data <= root->data) {
-                cur = insert(root->left, data);
-                root->left = cur;
-            }
-            else {
-                cur = insert(root->right, data);
-                root->right = cur;
-            }
-
-            return root;
-        }
-    }
-
-    static int get_height(Node* root)
-    {
-        static int current_height {}, max_height {};
-
-        if (root->left || root->right) {
-            ++current_height;
-
-            if (root->left)
-                get_height(root->left);
-            if (root->right)
-                get_height(root->right);
-
-            --current_height;
-        }
-        else if (current_height > max_height)
-            max_height = current_height;
-
-        return max_height;
-    }
-};
+shared_ptr<Node> read_tree();
+shared_ptr<Node> insert_node(shared_ptr<Node> root, int data);
+int get_height(const shared_ptr<Node>& root);
+void get_height(const shared_ptr<Node>& root, int current_height, const shared_ptr<int>& max_height);
+bool is_leaf(const shared_ptr<Node>& root);
 
 int main()
 {
-    Solution my_tree;
-    Node* root {nullptr};
+    shared_ptr root{read_tree()};
+    cout << get_height(root);
+    return 0;
+}
+
+shared_ptr<Node> read_tree()
+{
     int n;
     cin >> n;
+    shared_ptr<Node> root;
+    for (int data; n-- && cin >> data;)
+        root = insert_node(root, data);
+    return root;
+}
 
-    for (int data; cin >> data; root = my_tree.insert(root, data));
-    cout << Solution::get_height(root);
+shared_ptr<Node> insert_node(shared_ptr<Node> root, int data)
+{
+    if (!root)
+        return make_shared<Node>(data);
 
-    return 0;
+    shared_ptr<Node> node;
+    if (data <= root->data) {
+        node = insert_node(root->left, data);
+        root->left = node;
+    } else {
+        node = insert_node(root->right, data);
+        root->right = node;
+    }
+    return root;
+}
+
+int get_height(const shared_ptr<Node>& root)
+{
+    auto max_height{make_shared<int>()};
+    get_height(root, 0, max_height);
+    return *max_height;
+}
+
+void get_height(const shared_ptr<Node>& root, int current_height, const shared_ptr<int>& max_height)
+{
+    if (!root)
+        return;
+    if (is_leaf(root)) {
+        *max_height = max(current_height, *max_height);
+        return;
+    }
+    get_height(root->left, current_height + 1, max_height);
+    get_height(root->right, current_height + 1, max_height);
+}
+
+bool is_leaf(const shared_ptr<Node>& root)
+{
+    return !root->left && !root->right;
 }
