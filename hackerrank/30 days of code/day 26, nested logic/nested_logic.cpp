62e5a4ff352a0be968e84c0f170e2a17df987f06
Fernando <fernando.zuher@gmail.com>
2025-04-18 16:57:26 -0300

    small refactoring

--- hackerrank/30 days of code/day 26, nested logic/nested_logic.cpp
+++ hackerrank/30 days of code/day 26, nested logic/nested_logic.cpp
@@ -6,36 +6,46 @@
 #include <tuple>
 
 using namespace std;
+using date = chrono::year_month_day;
 
-tuple<chrono::year_month_day, chrono::year_month_day> read_dates();
-int calculate_fine(const chrono::year_month_day& returned_real_date, const chrono::year_month_day& due_date);
+tuple<date, date> read_dates();
+int calculate_fine(const date& returned_real_date, const date& due_date);
 
 int main()
 {
     auto [returned_real_date, due_date] = read_dates();
-    cout << (returned_real_date <= due_date ? 0 : calculate_fine(returned_real_date, due_date));
+    cout << calculate_fine(returned_real_date, due_date);
     return 0;
 }
 
-tuple<chrono::year_month_day, chrono::year_month_day> read_dates()
+tuple<date, date> read_dates()
 {
     int d1, m1, y1, d2, m2, y2;
     cin >> d1 >> m1 >> y1;
     cin >> d2 >> m2 >> y2;
-    return {
-        chrono::year_month_day{chrono::year{y1}, chrono::month(m1), chrono::day(d1)},
-        chrono::year_month_day{chrono::year{y2}, chrono::month(m2), chrono::day(d2)}
-    };
+
+    auto returned_date{date{chrono::year{y1}, chrono::month(m1), chrono::day(d1)}};
+    auto due_date{date{chrono::year{y2}, chrono::month(m2), chrono::day(d2)}};
+    return {returned_date, due_date};
 }
 
-int calculate_fine(const chrono::year_month_day& returned_real_date, const chrono::year_month_day& due_date)
+int calculate_fine(const date& returned_real_date, const date& due_date)
 {
-    constexpr int per_day = 15,
-            per_month = 500,
-            per_year = 10000;
-    if (returned_real_date.year() > due_date.year())
+    constexpr int per_day = 15;
+    constexpr int per_month = 500;
+    constexpr int per_year = 10000;
+
+    if (returned_real_date <= due_date)
+        return 0;
+
+    if (bool is_late_by_year{returned_real_date.year() > due_date.year()}; is_late_by_year)
         return per_year;
-    if (returned_real_date.year() == due_date.year() && returned_real_date.month() > due_date.month())
+
+    if (bool is_late_by_month{
+        returned_real_date.year() == due_date.year() && returned_real_date.month() > due_date.month()
+    }; is_late_by_month)
         return static_cast<int>((returned_real_date.month() - due_date.month()).count()) * per_month;
+
+    // else is_late_by_day
     return static_cast<int>((returned_real_date.day() - due_date.day()).count()) * per_day;
 }
