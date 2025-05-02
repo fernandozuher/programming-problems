dec828cec94f94f6114c91eba3d4254f41856757
Fernando <fernando.zuher@gmail.com>
2025-05-01 21:18:48 -0300

    refactored

--- hackerrank/30 days of code/day 27, testing/testing.py
+++ hackerrank/30 days of code/day 27, testing/testing.py
@@ -1,96 +1,32 @@
 # https://www.hackerrank.com/challenges/30-testing/problem?isFullScreen=true
 
-def main():
-
-    test_with_empty_array()
-    test_with_unique_values()
-    test_with_exacty_two_different_minimums()
-    print("OK")
+import unittest
 
 
-def test_with_empty_array():
+class MinimumIndexTests(unittest.TestCase):
+    def test_empty_array(self):
+        numbers = []
+        with self.assertRaises(ValueError):
+            minimum_index_of_smallest_element(numbers)
 
-    try:
-        seq = TestDataEmptyArray.get_array()
-        result = minimum_index(seq)
-    except ValueError as e:
-        pass
-    else:
-        assert False
+    def test_unique_values(self):
+        numbers = [2, 1]
+        self.assertEqual(minimum_index_of_smallest_element(numbers), 1)
+        self.assertEqual(len(set(numbers)), len(numbers), "Array elements are not unique.")
 
-
-def minimum_index(seq):
-
-    if len(seq) == 0:
-        raise ValueError("Cannot get the minimum value index from an empty sequence")
-
-    min_idx = 0
-    for i in range(1, len(seq)):
-        if seq[i] < seq[min_idx]:
-            min_idx = i
-
-    return min_idx
+    def test_two_minimum_values(self):
+        numbers = [1, 2, 1]
+        result = minimum_index_of_smallest_element(numbers)
+        self.assertEqual(result, 0)
+        self.assertEqual(numbers.count(min(numbers)), 2,
+                         "There should be exactly two occurrences of the minimum value.")
 
 
-def test_with_unique_values():
-
-    seq = TestDataUniqueValues.get_array()
-    assert len(seq) >= 2
-
-    assert len(list(set(seq))) == len(seq)
-
-    expected_result = TestDataUniqueValues.get_expected_result()
-    result = minimum_index(seq)
-    assert result == expected_result
-
-
-def test_with_exacty_two_different_minimums():
-
-    seq = TestDataExactlyTwoDifferentMinimums.get_array()
-    assert len(seq) >= 2
-    tmp = sorted(seq)
-    assert tmp[0] == tmp[1] and (len(tmp) == 2 or tmp[1] < tmp[2])
-
-    expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result()
-    result = minimum_index(seq)
-    assert result == expected_result
-
-
-class TestDataEmptyArray:
-
-    @staticmethod
-    def get_array():
-
-        return []
-
-
-class TestDataUniqueValues:
-
-    @staticmethod
-    def get_array():
-
-        return [2, 1]
-
-
-    @staticmethod
-    def get_expected_result():
-
-        return 1
-
-
-class TestDataExactlyTwoDifferentMinimums:
-
-    @staticmethod
-    def get_array():
-
-        return [1, 2, 1]
-
-
-    @staticmethod
-    def get_expected_result():
-
-        return 0
+def minimum_index_of_smallest_element(numbers):
+    if not numbers:
+        raise ValueError("Cannot get the minimum value index from an empty sequence.")
+    return numbers.index(min(numbers))
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
