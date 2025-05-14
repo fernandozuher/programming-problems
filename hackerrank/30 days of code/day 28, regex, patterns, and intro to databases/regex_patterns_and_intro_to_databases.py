c4272d3606b7156af11efc998c29e368cb49bc99
Fernando <fernando.zuher@gmail.com>
2025-05-14 07:21:34 -0300

    undone last change in wrong file

--- hackerrank/30 days of code/day 28, regex, patterns, and intro to databases/regex_patterns_and_intro_to_databases.py
+++ hackerrank/30 days of code/day 28, regex, patterns, and intro to databases/regex_patterns_and_intro_to_databases.py
@@ -1,16 +1,21 @@
-# https://www.hackerrank.com/challenges/30-bitwise-and/problem?isFullScreen=true
+# https://www.hackerrank.com/challenges/30-regex-patterns/problem?isFullScreen=true
 
 def main():
-    test_cases = int(input())
-    for _ in range(test_cases):
-        n, k = list(map(int, input().split()))
-        print(bitwise_and(n, k))
+    n = int(input())
+    names = collect_gmail_users(n)
+    names.sort()
+    print(*names, sep='\n')
 
 
-def bitwise_and(n, k):
-    if (k - 1 | k) <= n:
-        return k - 1
-    return k - 2
+def collect_gmail_users(n):
+    names = []
+
+    for _ in range(n):
+        name, email = input().split()
+        if email.endswith('@gmail.com'):
+            names.append(name)
+
+    return names
 
 
 if __name__ == '__main__':
