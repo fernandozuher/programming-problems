f3ca2cb020a705cc8ccd06f654b8aa5228482981
Fernando <fernando.zuher@gmail.com>
2025-07-14 15:32:10 -0300

    refactor picking_numbers

--- hackerrank/problem solving/algorithms/2 - implementation/17 - picking numbers/picking_numbers.py
+++ hackerrank/problem solving/algorithms/2 - implementation/17 - picking numbers/picking_numbers.py
@@ -1,80 +1,23 @@
 # https://www.hackerrank.com/challenges/picking-numbers/problem?isFullScreen=true
 
+from collections import Counter
+
+
 def main():
-
-    n = int(input())
-    array = read_int_array()
-    array.sort()
-
-    obj = PickingNumbers(array)
-    print(obj.subarray_biggest_size())
+    input()
+    print(picking_numbers(read_numbers()))
 
 
-def read_int_array():
-
+def read_numbers():
     return list(map(int, input().split()))
 
 
-class PickingNumbers:
-
-    def __init__(self, numbers):
-
-        self._numbers = numbers
-        self._subarray_biggest_size = 1
-        self._picking_numbers()
-
-
-    def _picking_numbers(self):
-
-        first_reference_number_index = 0
-        second_reference_number_index = 0
-        subarray_current_size = 1
-
-        for i in range(1, len(self._numbers)):
-            difference = self._numbers[i] - self._numbers[first_reference_number_index]
-
-            match difference:
-                case 0:
-                    subarray_current_size += 1
-
-                case 1:
-                    subarray_current_size += 1
-                    if self._numbers[second_reference_number_index] != self._numbers[i]:
-                        second_reference_number_index = i
-
-                case _:
-                    self._subarray_biggest_size = self._update_subarray_biggest_size(subarray_current_size)
-                    update = self._update_first_reference_number_index_and_subarray_current_size(i, second_reference_number_index)
-
-                    first_reference_number_index = update[0]
-                    second_reference_number_index = i
-                    subarray_current_size = update[1]
-
-        self._subarray_biggest_size = self._update_subarray_biggest_size(subarray_current_size)
-
-
-    def _update_subarray_biggest_size(self, subarray_current_size):
-
-        return max(subarray_current_size, self._subarray_biggest_size)
-
-
-    def _update_first_reference_number_index_and_subarray_current_size(self, i, second_reference_number_index):
-
-        first_reference_number_index, subarray_current_size = 0, 0
-
-        if self._numbers[i] - self._numbers[second_reference_number_index] == 1:
-            first_reference_number_index = second_reference_number_index
-            subarray_current_size = i - second_reference_number_index + 1
-        else:
-            first_reference_number_index = i
-            subarray_current_size = 1
-
-        return [first_reference_number_index, subarray_current_size]
-
-
-    def subarray_biggest_size(self):
-
-        return self._subarray_biggest_size
+def picking_numbers(numbers):
+    counter = Counter(numbers)
+    max_len = 0
+    for num in counter:
+        max_len = max(max_len, counter[num] + counter.get(num + 1, 0))
+    return max_len
 
 
 if __name__ == '__main__':
--- hackerrank/problem solving/algorithms/2 - implementation/17 - picking numbers/picking_numbers.rb
+++ hackerrank/problem solving/algorithms/2 - implementation/17 - picking numbers/picking_numbers.rb
@@ -1,75 +1,17 @@
 # https://www.hackerrank.com/challenges/picking-numbers/problem?isFullScreen=true
 
-def main
-    n = gets.to_i
-    array = read_int_array
-    array.sort!
-
-    obj = PickingNumbers.new(array)
-    puts obj.subarray_biggest_size
+def main()
+  gets
+  puts picking_numbers(read_numbers)
 end
 
-    def read_int_array
-        gets.split.map(&:to_i)
-    end
+def read_numbers
+  gets.split.map(&:to_i)
+end
 
-    class PickingNumbers
-        attr_reader :subarray_biggest_size
-
-        def initialize numbers
-            @numbers = numbers
-            @subarray_biggest_size = 1
-            picking_numbers
-        end
-
-            private def picking_numbers
-                first_reference_number_index = 0
-                second_reference_number_index = 0
-                subarray_current_size = 1
-
-                for i in 1..(@numbers.size - 1)
-                    difference = @numbers[i] - @numbers[first_reference_number_index]
-
-                    case difference
-                        when 0
-                            subarray_current_size += 1
-
-                        when 1
-                            subarray_current_size += 1
-                            if @numbers[second_reference_number_index] != @numbers[i]
-                                second_reference_number_index = i
-                            end
-
-                        else
-                            @subarray_biggest_size = update_subarray_biggest_size(subarray_current_size)
-                            update = update_first_reference_number_index_and_subarray_current_size(i, second_reference_number_index)
-
-                            first_reference_number_index = update.first
-                            second_reference_number_index = i
-                            subarray_current_size = update.last
-                    end
-                end
-
-                @subarray_biggest_size = update_subarray_biggest_size(subarray_current_size)
-            end
-
-                private def update_subarray_biggest_size(subarray_current_size)
-                    [subarray_current_size, @subarray_biggest_size].max
-                end
-
-                private def update_first_reference_number_index_and_subarray_current_size(i, second_reference_number_index)
-                    first_reference_number_index, subarray_current_size = 0, 0
-
-                    if @numbers[i] - @numbers[second_reference_number_index] == 1
-                        first_reference_number_index = second_reference_number_index
-                        subarray_current_size = i - second_reference_number_index + 1
-                    else
-                        first_reference_number_index = i
-                        subarray_current_size = 1
-                    end
-
-                    [first_reference_number_index, subarray_current_size]
-                end
-    end
+def picking_numbers(numbers)
+  counter = numbers.tally
+  counter.each_key.inject(0) { |max_len, num| [max_len, counter[num] + (counter[num + 1] || 0)].max }
+end
 
 main
