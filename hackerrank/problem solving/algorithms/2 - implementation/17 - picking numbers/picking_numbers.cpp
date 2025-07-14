ba410becd38d675bd5d02b8ecbac6f018d308b9b
Fernando <fernando.zuher@gmail.com>
2025-07-14 16:01:47 -0300

    refactor picking_numbers

--- hackerrank/problem solving/algorithms/2 - implementation/17 - picking numbers/picking_numbers.cpp
+++ hackerrank/problem solving/algorithms/2 - implementation/17 - picking numbers/picking_numbers.cpp
@@ -1,78 +1,45 @@
 // https://www.hackerrank.com/challenges/picking-numbers/problem?isFullScreen=true
-// From C++23 onwards
 
 #include <algorithm>
 #include <iostream>
-#include <ranges>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-template <class T = int>
-vector<T> read_array(int n);
+vector<int> read_numbers(int n);
 int picking_numbers(const vector<int>& numbers);
-tuple<int, int, int> update_indexes_and_size(
-    const vector<int>& numbers, int i, int second_reference_number_index);
+
+using namespace std;
 
 int main()
 {
     int n;
     cin >> n;
-    vector<int> array{read_array(n)};
-    ranges::sort(array);
-    cout << picking_numbers(array);
+    vector<int> numbers{read_numbers(n)};
+    cout << picking_numbers(numbers);
     return 0;
 }
 
-template <class T>
-vector<T> read_array(const int n)
+vector<int> read_numbers(int n)
 {
-    vector<T> array(n);
-    ranges::generate(array, [] {T x; cin >> x; return x;});
-    return array;
+    vector<int> numbers(n);
+    for (auto& x : numbers)
+        cin >> x;
+    return numbers;
 }
 
 int picking_numbers(const vector<int>& numbers)
 {
-    int subarray_biggest_length{1};
-    int subarray_current_size{1};
+    unordered_map<int, int> counter;
+    for (int num : numbers)
+        ++counter[num];
 
-    for (int first_reference_number_index{}, second_reference_number_index{};
-         const auto [i, number] : numbers | views::enumerate | views::drop(1)) {
-        switch (int difference{number - numbers.at(first_reference_number_index)}) {
-        case 0:
-            ++subarray_current_size;
-            break;
-
-        case 1:
-            ++subarray_current_size;
-            if (numbers.at(second_reference_number_index) != number)
-                second_reference_number_index = i;
-            break;
-
-        default:
-            subarray_biggest_length = max(subarray_current_size, subarray_biggest_length);
-            tie(first_reference_number_index, second_reference_number_index, subarray_current_size) =
-                update_indexes_and_size(numbers, i, second_reference_number_index);
-        }
+    int max_len{};
+    for (auto [num, count] : counter) {
+        int current{count + (counter.contains(num + 1) ? counter[num + 1] : 0)};
+        max_len = max(max_len, current);
     }
 
-    return max(subarray_biggest_length, subarray_current_size);
-}
-
-tuple<int, int, int> update_indexes_and_size(
-    const vector<int>& numbers, const int i, const int second_reference_number_index)
-{
-    int first_reference_number_index, subarray_current_size;
-
-    if (numbers.at(i) - numbers.at(second_reference_number_index) == 1) {
-        first_reference_number_index = second_reference_number_index;
-        subarray_current_size = i - second_reference_number_index + 1;
-    }
-    else {
-        first_reference_number_index = i;
-        subarray_current_size = 1;
-    }
-
-    return {first_reference_number_index, i, subarray_current_size};
+    return max_len;
 }
