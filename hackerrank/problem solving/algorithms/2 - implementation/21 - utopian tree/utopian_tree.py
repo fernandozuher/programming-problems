d353c344287496be5a3542264f8bfcee4e43323e
Fernando <fernando.zuher@gmail.com>
2025-07-31 10:24:20 -0300

    refactor utopian_tree

--- hackerrank/problem solving/algorithms/2 - implementation/21 - utopian tree/utopian_tree.py
+++ hackerrank/problem solving/algorithms/2 - implementation/21 - utopian tree/utopian_tree.py
@@ -1,50 +1,29 @@
 # https://www.hackerrank.com/challenges/utopian-tree/problem?isFullScreen=true
 
 def main():
-
     n = int(input())
-    test_cases = read_lines(n)
-    obj = UtopianTree(test_cases)
-    print(*obj.trees_heights(), sep='\n')
+    cycles = read_numbers(n)
+    print(*utopian_tree(cycles), sep='\n')
 
 
-def read_lines(n):
-
+def read_numbers(n):
     return [int(input()) for _ in range(n)]
 
 
-class UtopianTree:
-
-    def __init__(self, test_cases):
-
-        self._test_cases = test_cases
-        self._trees_heights = [None] * len(test_cases)
-        self._calculate_trees_heights()
+def utopian_tree(cycles):
+    return list(map(calculate_height, cycles))
 
 
-    def _calculate_trees_heights(self):
-
-        for i in range(len(self._trees_heights)):
-            self._trees_heights[i] = self._calculate_height(self._test_cases[i])
-
-
-    def _calculate_height(self, cycles):
-
-        height = 1
-        for cycle in range(1, cycles+1):
-            height = height * 2 if self._is_cycle_happening_during_spring(cycle) else height + 1
-        return height
+def calculate_height(cycles):
+    height = 1
+    for cycle in range(1, cycles + 1):
+        height = height * 2 if is_cycle_happening_during_spring(cycle) else height + 1
+    return height
 
 
-    def _is_cycle_happening_during_spring(self, cycle):
-
-        return cycle & 1
-
-
-    def trees_heights(self):
-
-        return self._trees_heights
+def is_cycle_happening_during_spring(cycle):
+    return cycle & 1
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- hackerrank/problem solving/algorithms/2 - implementation/21 - utopian tree/utopian_tree.rb
+++ hackerrank/problem solving/algorithms/2 - implementation/21 - utopian tree/utopian_tree.rb
@@ -1,42 +1,25 @@
 # https://www.hackerrank.com/challenges/utopian-tree/problem?isFullScreen=true
 
 def main
-    n = gets.to_i
-    test_cases = read_lines(n)
-    obj = Utopian_tree.new(test_cases)
-    puts obj.trees_heights
+  n = gets.to_i
+  test_cases = read_numbers(n)
+  puts utopian_tree(test_cases)
 end
 
-    def read_lines(n)
-        Array.new(n).map{gets.to_i}
-    end
+def read_numbers(n)
+  Array.new(n) { gets.to_i }
+end
 
-    class Utopian_tree
-        attr_reader :trees_heights
+def utopian_tree(test_cases)
+  test_cases.map { |cycles| calculate_height(cycles) }
+end
 
-        def initialize(test_cases)
-            @test_cases = test_cases
-            @trees_heights = Array.new(@test_cases.size)
-            calculate_trees_heights
-        end
+def calculate_height(cycles)
+  (1...cycles + 1).inject(1) { |height, cycle| is_cycle_happening_during_spring(cycle) == true ? height * 2 : height + 1 }
+end
 
-            private def calculate_trees_heights
-                for i in 0..(@trees_heights.size-1)
-                    @trees_heights[i] = calculate_height(@test_cases[i])
-                end
-            end
+def is_cycle_happening_during_spring(cycle)
+  cycle.odd?
+end
 
-                private def calculate_height(cycles)
-                    height = 1
-                    for cycle in 1..cycles
-                        height = is_cycle_happening_during_spring(cycle) == true ? height * 2 : height + 1
-                    end
-                    height
-                end
-
-                    def is_cycle_happening_during_spring(cycle)
-                        cycle.odd?
-                    end
-    end
-
-main
+main if __FILE__ == $PROGRAM_NAME
