233530b79cce9c0aa0d2eeec8fc5b0dead5a490a
Fernando <fernando.zuher@gmail.com>
2025-12-08 09:49:24 -0300

    refactor repeated_string

--- hackerrank/problem solving/algorithms/2 - implementation/36 - repeated string/repeated_string.c
+++ hackerrank/problem solving/algorithms/2 - implementation/36 - repeated string/repeated_string.c
@@ -5,41 +5,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-long count_in_repeated_string(const char *string, char letter, long n_characters);
-long count(const char *string, long n, char letter);
+long long count_in_repeated_string(const char *string, char letter, long long n_characters);
+long long count(const char *string, long long n, char letter);
 
 int main()
 {
     constexpr int string_max_size = 102; // 100 + \n + \0
     char string[string_max_size];
-    long n_characters;
+    long long n_characters;
 
     fgets(string, sizeof(string), stdin);
     string[strlen(string) - 1] = '\0';
-    scanf("%ld", &n_characters);
+    scanf("%lld", &n_characters);
 
     constexpr char letter = 'a';
-    printf("%ld\n", count_in_repeated_string(string, letter, n_characters));
+    printf("%lld\n", count_in_repeated_string(string, letter, n_characters));
 
     return 0;
 }
 
-long count_in_repeated_string(const char *string, char letter, long n_characters)
+long long count_in_repeated_string(const char *string, char letter, long long n_characters)
 {
-    long n = strlen(string);
-    ldiv_t division = ldiv(n_characters, n);
-    long full_repeats = division.quot;
-    long n_substring = division.rem;
+    auto n = (long long) strlen(string);
+    lldiv_t division = lldiv(n_characters, n);
+    long long full_repeats = division.quot;
+    long long n_substring = division.rem;
 
-    long quantity = count(string, n, letter);
+    long long quantity = count(string, n, letter);
     quantity *= full_repeats;
     return quantity + count(string, n_substring, letter);
 }
 
-long count(const char *string, long n, char letter)
+long long count(const char *string, long long n, char letter)
 {
-    long quantity = 0;
-    for (long i = 0; string[i] && i < n; ++i)
+    long long quantity = 0;
+    for (long long i = 0; string[i] && i < n; ++i)
         quantity += string[i] == letter;
     return quantity;
 }
