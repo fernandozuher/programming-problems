b80452d07bba1317f15542918ef0a8f094e2731d
Fernando <fernando.zuher@gmail.com>
2026-02-19 12:52:10 -0300

    added complexity analysis, refactoring

--- hackerrank/problem solving/algorithms/2 - implementation/55 - cavity map/cavity_map.py
+++ hackerrank/problem solving/algorithms/2 - implementation/55 - cavity map/cavity_map.py
@@ -8,19 +8,24 @@ def main():
         print(''.join(line))
 
 
+# n: length of rows/cols of matrix
+# T: O(n^2)
+# S: O(n^2) extra space
 def change_matrix_to_cavity_map(matrix):
     subrange = range(1, len(matrix) - 1)
-    for i in subrange:
-        for j in subrange:
-            if is_cell_cavity(matrix, i, j):
-                matrix[i][j] = 'X'
+    cavities = [(i, j) for i in subrange for j in subrange if is_cell_cavity(matrix, i, j)]
+    for i, j in cavities:
+        matrix[i][j] = 'X'
 
 
 def is_cell_cavity(matrix, i, j):
-    return matrix[i - 1][j] < matrix[i][j] \
-        and matrix[i][j - 1] < matrix[i][j] \
-        and matrix[i + 1][j] < matrix[i][j] \
-        and matrix[i][j + 1] < matrix[i][j]
+    indexes = valid_indexes(i, j)
+    cell = matrix[i][j]
+    return all(matrix[r][c] < cell for r, c in indexes)
+
+
+def valid_indexes(i, j):
+    return [(i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1)]
 
 
 if __name__ == '__main__':
