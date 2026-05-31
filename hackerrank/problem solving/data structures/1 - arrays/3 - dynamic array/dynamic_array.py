db2110367fa64ed1843475fbf9e3ef1e0c12776c
Fernando <fernando.zuher@gmail.com>
2026-05-31 19:01:54 -0300

    refactoring

--- hackerrank/problem solving/data structures/1 - arrays/3 - dynamic array/dynamic_array.py
+++ hackerrank/problem solving/data structures/1 - arrays/3 - dynamic array/dynamic_array.py
@@ -1,45 +1,39 @@
 # https://www.hackerrank.com/challenges/dynamic-array/problem?isFullScreen=true
 
 def main():
-
-    n, n_queries = read_int_array()
+    n, n_queries = read_numbers()
     obj = DynamicArray(n)
 
     for _ in range(n_queries):
-        obj.handle_query(read_int_array())
+        obj.handle_query(read_numbers())
+
     print(*obj.answers(), sep='\n')
 
 
-def read_int_array():
-
+def read_numbers():
     return list(map(int, input().split()))
 
 
 class DynamicArray:
 
     def __init__(self, n):
-
         self._n = n
-        self._array = [[] for _ in range(n)]
+        self._arr = [[] for _ in range(n)]
         self._answers = []
         self._last_answer = 0
 
-
     def handle_query(self, query):
-
         type, x, y = query
         index = (x ^ self._last_answer) % self._n
 
         if type == 1:
-            self._array[index].append(y)
+            self._arr[index].append(y)
         else:
-            j = y % len(self._array[index])
-            self._last_answer = self._array[index][j]
+            j = y % len(self._arr[index])
+            self._last_answer = self._arr[index][j]
             self._answers.append(self._last_answer)
 
-
     def answers(self):
-
         return self._answers
 
 
