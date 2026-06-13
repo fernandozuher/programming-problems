3ba512144443bd94da33f37affd56a4187c80c5e
Fernando <fernando.zuher@gmail.com>
2026-06-12 22:29:36 -0300

    refactoring

--- hackerrank/problem solving/data structures/2 - linked lists/1 - singly linked list/2 - insert a node at the tail of a linked list/insert_a_node_at_the_tail_of_a_linked_list.cpp
+++ hackerrank/problem solving/data structures/2 - linked lists/1 - singly linked list/2 - insert a node at the tail of a linked list/insert_a_node_at_the_tail_of_a_linked_list.cpp
@@ -1,18 +1,27 @@
 // https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem?isFullScreen=true
+// C++23
 
-#include <algorithm>
-#include <forward_list>
-#include <iostream>
-
+import std;
 using namespace std;
+
+template<template<class ...> class C, class T>
+C<T> read_numbers(int n);
 
 int main()
 {
     int n;
     cin >> n;
-    forward_list<int> list(n);
-    ranges::generate(list, [] {int x; cin >> x; return x;});
-    ranges::for_each(list, [](const auto x) {cout << x << '\n';});
+    auto list{ read_numbers<forward_list, int>(n) };
+    for (auto x : list)
+        println("{}", x);
 
     return 0;
+}
+
+// T: O(n)
+// S: O(n) extra space
+template<template<class ...> class C, class T>
+C<T> read_numbers(int n)
+{
+    return views::iota(0, n) | views::transform([](auto) {T x; cin >> x; return x;}) | ranges::to<C<T>>();
 }
