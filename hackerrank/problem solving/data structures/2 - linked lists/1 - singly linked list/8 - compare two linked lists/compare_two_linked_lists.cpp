ff8f196171c4db36760e4dfc46e72df7bbdf8fe6
Fernando <fernando.zuher@gmail.com>
2026-06-22 20:20:04 -0300

    concepts added to functions

--- hackerrank/problem solving/data structures/2 - linked lists/1 - singly linked list/8 - compare two linked lists/compare_two_linked_lists.cpp
+++ hackerrank/problem solving/data structures/2 - linked lists/1 - singly linked list/8 - compare two linked lists/compare_two_linked_lists.cpp
@@ -1,45 +1,53 @@
 // https://www.hackerrank.com/challenges/compare-two-linked-lists/problem?isFullScreen=true
+// C++26
 
-#include <algorithm>
-#include <forward_list>
-#include <iostream>
-#include <vector>
-
+import std;
 using namespace std;
 
-forward_list<int> initialize_list(const int n);
-
 template<class T>
-void print_array(const vector<T>& array);
+concept Primitive = is_fundamental_v<T>;
+template<Primitive T>
+T read()
+{
+    T x;
+    cin >> x;
+    return x;
+}
+
+template<ranges::range C, Primitive T>
+C read(int n)
+{
+    return views::iota(0, n)
+        | views::transform([](auto) {return read<T>();})
+        | ranges::to<C>();
+}
+
+template<Primitive T>
+pair<forward_list<T>, forward_list<T>> read_2_lists()
+{
+    auto n{ read<T>() };
+    forward_list<T> l1{ read<forward_list<T>, T>(n) };
+    n = read<T>();
+    forward_list<T> l2{ read<forward_list<T>, T>(n) };
+    return { l1, l2 };
+}
+
+template<ranges::range C>
+void println(const C& container)
+{
+    for (const auto& x : container)
+        println("{}", x);
+}
 
 int main()
 {
-    int test_cases;
-    cin >> test_cases;
-    vector<bool> are_lists_equals(test_cases);
+    int n{ read<int>() };
 
-    for (int n{}; auto&& x : are_lists_equals) {
-        cin >> n;
-        forward_list<int> list1 {initialize_list(n)};
-        cin >> n;
-        forward_list<int> list2 {initialize_list(n)};
-        x = list1 == list2;
-    }
-
-    print_array(are_lists_equals);
+    println(
+        views::iota(0, n)
+        | views::transform([](auto) { auto [l1, l2] { read_2_lists<int>() }; return l1 == l2; })
+        | ranges::to<vector<int>>()
+    );
 
     return 0;
 }
-
-    forward_list<int> initialize_list(const int n)
-    {
-        forward_list<int> list(n);
-        ranges::generate(list, [] {int x; cin >> x; return x;});
-        return list;
-    }
-
-    template<class T>
-    void print_array(const vector<T>& array)
-    {
-        ranges::for_each(array, [](const auto x) {cout << x << '\n';});
-    }
