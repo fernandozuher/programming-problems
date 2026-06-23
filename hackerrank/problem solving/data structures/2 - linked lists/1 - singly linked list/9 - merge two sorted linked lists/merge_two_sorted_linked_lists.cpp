9ae699ff0fb85a7566ea4a2dcfcfb37312d5a1ff
Fernando <fernando.zuher@gmail.com>
2026-06-23 19:21:08 -0300

    refactoring

--- hackerrank/problem solving/data structures/2 - linked lists/1 - singly linked list/9 - merge two sorted linked lists/merge_two_sorted_linked_lists.cpp
+++ hackerrank/problem solving/data structures/2 - linked lists/1 - singly linked list/9 - merge two sorted linked lists/merge_two_sorted_linked_lists.cpp
@@ -1,47 +1,62 @@
 // https://www.hackerrank.com/challenges/merge-two-sorted-linked-lists/problem?isFullScreen=true
+// C++26
 
-#include <algorithm>
-#include <forward_list>
-#include <iostream>
-#include <vector>
-
+import std;
 using namespace std;
 
-forward_list<int> initialize_list(const int n);
-void print_list(const forward_list<int>& list);
+template<class T>
+concept Primitive = is_fundamental_v<T>;
+template<Primitive T>
+T read()
+{
+    T x;
+    cin >> x;
+    return x;
+}
+
+template<ranges::range C, Primitive T>
+C read(int n)
+{
+    return views::iota(0, n)
+        | views::transform([](auto) {return read<T>();})
+        | ranges::to<C>();
+}
+
+template<Primitive T>
+pair<forward_list<T>, forward_list<T>> read_2_lists()
+{
+    auto n{ read<T>() };
+    auto l1 = read<forward_list<T>, T>(n);
+    n = read<int>();
+    auto l2 = read<forward_list<T>, T>(n);
+    return { l1, l2 };
+}
+
+template<ranges::range C>
+void print(const C& container)
+{
+    for (const auto& x : container)
+        print("{} ", x);
+}
 
 int main()
 {
-    int test_cases;
-    cin >> test_cases;
-    vector<forward_list<int>> merged_lists(test_cases);
+    auto n{ read<int>() };
 
-    for (int n{}; auto& x : merged_lists) {
-        cin >> n;
-        forward_list<int> list1 {initialize_list(n)};
-        cin >> n;
-        forward_list<int> list2 {initialize_list(n)};
+    auto lists{
+        views::iota(0, n)
+        | views::transform([](auto) {
+            auto [l1, l2] { read_2_lists<int>() };
+            l1.merge(l2);
+            return l1;
+            })
+        | ranges::to<vector<forward_list<int>>>()
+    };
 
-        list1.merge(list2);
-        x = list1;
-    }
-
-    for (const auto& list : merged_lists) {
-        print_list(list);
-        cout << '\n';
+    for (const auto& l : lists) {
+        print(l);
+        println();
     }
 
     return 0;
 }
-
-    forward_list<int> initialize_list(const int n)
-    {
-        forward_list<int> list(n);
-        ranges::generate(list, []{int x; cin >> x; return x;});
-        return list;
-    }
-
-    void print_list(const forward_list<int>& list)
-    {
-        ranges::for_each(list, [](const auto x) {cout << x << ' ';});
-    }
