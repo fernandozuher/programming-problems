534a944bcef85a7824dd37a027ede9cd6ab74026
Fernando <fernando.zuher@gmail.com>
2026-06-26 17:05:51 -0300

    fix: detect merge point using shared node addresses

--- hackerrank/problem solving/data structures/2 - linked lists/1 - singly linked list/13 - find merge point of two lists/find_merge_point_of_two_lists.cpp
+++ hackerrank/problem solving/data structures/2 - linked lists/1 - singly linked list/13 - find merge point of two lists/find_merge_point_of_two_lists.cpp
@@ -1,68 +1,98 @@
 // https://www.hackerrank.com/challenges/find-the-merge-point-of-two-joined-linked-lists/problem?isFullScreen=true
-// C++26
+// C++23
 
-// BAD INPUT COMPOSED (ADAPTED FROM) BY HACKERRANK SOURCE
+// This solution is better than the expected one for this problem, although it won't run on the platform,
+// only locally with custom test cases.
+// The problem statement is confusing and contains contradictory information about some constraints.
 
 import std;
 using namespace std;
 
 template<class T>
 concept Primitive = is_fundamental_v<T>;
+
 template<Primitive T>
-T read()
+struct Node {
+    T data;
+    shared_ptr<Node<T>> next;
+};
+
+using two_nodes = pair<shared_ptr<Node<int>>, shared_ptr<Node<int>>>;
+
+two_nodes test_case_point_at_2()
 {
-    T x;
-    cin >> x;
-    return x;
+    auto head1{ make_shared<Node<int>>(1) };
+    auto node2{ make_shared<Node<int>>(2) };
+    auto node3{ make_shared<Node<int>>(3) };
+    head1->next = node2;
+    node2->next = node3;
+
+    auto head2{ make_shared<Node<int>>(1) };
+    head2->next = node2;
+
+    return { head1, head2 };
 }
 
-template<ranges::range C, class T>
-C read(int n)
+two_nodes test_case_point_at_3()
 {
-    return views::iota(0, n)
-        | views::transform([](auto) { return read<T>(); })
-        | ranges::to<C>();
+    auto head1{ make_shared<Node<int>>(1) };
+    auto node2{ make_shared<Node<int>>(2) };
+    auto node3{ make_shared<Node<int>>(3) };
+    head1->next = node2;
+    node2->next = node3;
+
+    auto head2{ make_shared<Node<int>>(1) };
+    head2->next = node3;
+
+    return { head1, head2 };
 }
 
-template<ranges::range C, class T>
-pair<C, C> read_2_containers()
+two_nodes test_case_no_merge()
 {
-    int n{ read<T>() };
-    auto container1{ read<C, T>(n) };
-    n = read<T>();
-    auto container2{ read<C, T>(n) };
-    return { container1, container2 };
+    auto head1{ make_shared<Node<int>>(1) };
+    auto node2{ make_shared<Node<int>>(2) };
+    auto node3{ make_shared<Node<int>>(3) };
+    head1->next = node2;
+    node2->next = node3;
+
+    auto head2{ make_shared<Node<int>>(1) };
+    auto node2_2{ make_shared<Node<int>>(2) };
+    auto node3_2{ make_shared<Node<int>>(3) };
+    head2->next = node2_2;
+    node2_2->next = node3_2;
+
+    return { head1, head2 };
 }
 
-// T: It depends on the container type. For forward_list is O(position)
-// S: O(1) extra space
-template<ranges::range C>
-auto value_at(const C& container, int position)
+// n_l1: length of l1
+// n_l2: length of l2
+// T: O(n_l1 + n_l2)
+// S: O(n_l1 + n_l2) extra space
+template<Primitive T>
+T find_merge_point(const shared_ptr<Node<T>>& l1, const shared_ptr<Node<T>>& l2)
 {
-    return *std::next(container.begin(), position);
-}
+    unordered_set<Node<T>*> addresses;
+    for (auto it1{ l1 }, it2{ l2 }; it1 || it2;) {
+        for (auto [i, it] : views::enumerate(array{ it1, it2 })) {
+            if (it) {
+                if (addresses.contains(it.get()))
+                    return it->data;
 
-template<ranges::range C>
-void println(const C& container)
-{
-    for (const auto& x : container)
-        print("{} ", x);
-    println();
+                addresses.insert(it.get());
+
+                if (i == 0)
+                    it1 = it1->next;
+                else
+                    it2 = it2->next;
+            }
+        }
+    }
+    return T{};
 }
 
 int main()
 {
-    int n{ read<int>() };
-    vector<int> merge_points(n);
-
-    for (auto& x : merge_points) {
-        int position{ read<int>() };
-        auto [l1, l2] { read_2_containers<forward_list<int>, int>() };
-        l1.merge(l2);
-        x = value_at(l1, position);
-    }
-
-    println(merge_points);
-
+    for (const auto& [head1, head2] : { test_case_point_at_2(), test_case_point_at_3(), test_case_no_merge() })
+        println("{}", find_merge_point(head1, head2));
     return 0;
 }
