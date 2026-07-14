20156d6668423c313f1d2643bf82c80e9c672bdf
Fernando <fernando.zuher@gmail.com>
2026-07-13 21:31:07 -0300

    refactor: generalize read function

--- hackerrank/problem solving/algorithms/1 - warmup/3 - compare the triplets/compare_the_triplets.py
+++ hackerrank/problem solving/algorithms/1 - warmup/3 - compare the triplets/compare_the_triplets.py
@@ -1,18 +1,31 @@
 # https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
 
+import sys
+from typing import Type, List, Any, TextIO
+
+
 def main():
-    triplet_a = read_numbers()
-    triplet_b = read_numbers()
+    triplet_a = read(int)
+    triplet_b = read(int)
     score_a, score_b = compare_triplets(triplet_a, triplet_b)
     print(f"{score_a} {score_b}")
 
 
-def read_numbers():
-    return list(map(int, input().split()))
+def read(target_type: Type[Any], stream: TextIO = sys.stdin) -> List[Any]:
+    if not is_callable_builtin_type(target_type):
+        raise TypeError(f"Type '{target_type}' is not a valid built-in type.")
+    line = stream.readline()
+    return list(map(target_type, line.strip().split()))
+
+
+def is_callable_builtin_type(target_type: Type[Any]):
+    return (hasattr(target_type, '__module__') and
+            target_type.__module__ == 'builtins' and
+            callable(target_type))
 
 
 # n: length of a and b = 3
-# T: O(3) = O(1)
+# T: O(3) => O(1)
 # S: O(1) extra space
 def compare_triplets(triplet_a, triplet_b):
     score_a, score_b = 0, 0
