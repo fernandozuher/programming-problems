7d17388a65a8f355c5e71252a6cf5393618c7c7e
Fernando <fernando.zuher@gmail.com>
2026-08-02 12:18:38 -0300

    refactor: use fully qualified text_io::read! macro

--- hackerrank/problem solving/algorithms/1 - warmup/1 - solve me first/solve_me_first.rs
+++ hackerrank/problem solving/algorithms/1 - warmup/1 - solve me first/solve_me_first.rs
@@ -1,10 +1,8 @@
 // https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
 
-use text_io::read;
-
 fn main() {
-    let n1: i32 = read!();
-    let n2: i32 = read!();
+    let n1: i32 = text_io::read!();
+    let n2: i32 = text_io::read!();
     print!("{}", solve_me_first(n1, n2));
 }
 
